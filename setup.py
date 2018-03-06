cbaccb7aef86a38f7b970317c92aa12796c4d59b
David Durst <davidbdurst@gmail.com>
2018-03-06 15:35:41 -0800

    cleaning up and adding first shot at some code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aetherling',
+    version='0.0.1',
+    description='The python component of Aetherling\'s, the library for data-parallel programming using the Stanford coreir and magma projects',
+    packages=["aetherling"],
+    license='BSD License',
+    url='https://github.com/David-Durst/aetherling',
+    author='David Durst',
+    author_email='durst@cs.stanford.edu'
+)
