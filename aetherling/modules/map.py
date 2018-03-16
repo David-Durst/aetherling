06073747bd136cae1f358b24410c4a7c92ca6071
David Durst <davidbdurst@gmail.com>
2018-03-15 23:24:15 -0700

    added map, documented dehydrate

--- aetherling/modules/map.py
+++ aetherling/modules/map.py
@@ -1,24 +1,16 @@
 from magma.backend.coreir_ import CoreIRBackend
 from magma.frontend.coreir_ import ModuleFromGeneratorWrapper
 
-__all__ = ["Dehydrate", "Hydrate"]
+__all__ = ["MapParallel", "MapSequential"]
 
-def Dehydrate(cirb: CoreIRBackend, T):
+def MapParallel(cirb: CoreIRBackend, NumInputs, Op):
     """
-    Convert a nested type to a flat array of bits
-    Aetherling Type: {1, T} -> {1, Bit[width(T)]}
+    Map an operation over NumInputs inputs
+    Aetherling Type: {1, T[NumInputs]} -> {1, S[width(T)]}
 
-    Args:
-        cirb: The CoreIR backend currently be used
-        T: The type to dehydrate
-
-    Returns:
-        A module with the following ports:
-        I : In(T)
-        O : Out(Array(width(T), Bit))
-
-        The module also has the following data:
-        size: width(T)
+    I : In(T)
+    O : Out(Array(width(T), Bit))
+    size: width(T)
     """
     cirType = cirb.get_type(T, True)
     moduleToReturn = ModuleFromGeneratorWrapper(cirb, "aetherlinglib", "dehydrate",
@@ -32,16 +24,9 @@ def Hydrate(cirb: CoreIRBackend, T):
     Convert a flat array of bits to a nested type
     Aetherling Type: {1, Bit[width(T)]} -> {1, T}
 
-    Args:
-        cirb: The CoreIR backend currently be used
-        T: The type to hydrate
-
-    Returns:
-        A module with the following ports:
-        I : In(Array(width(T), BitIn))
-        O : In(T)
-        The module also has the following data:
-        size: width(T)
+    I : In(Array(width(T), BitIn))
+    O : In(T)
+    size: width(T)
     """
     cirType = cirb.get_type(T, True)
     moduleToReturn = ModuleFromGeneratorWrapper(cirb, "aetherlinglib", "hydrate",
