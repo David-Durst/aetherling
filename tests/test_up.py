51c8da1dd0cab4221e7e5b0e1bd9d39aed0349f6
David Durst <davidbdurst@gmail.com>
2018-03-31 14:17:37 -0700

    removing garbage from upsample test

--- tests/test_up.py
+++ tests/test_up.py
@@ -13,40 +13,6 @@ from magma.scope import Scope
 from magma.simulator.mdb import simulate
 from mantle import CounterModM, Decode
 
-
-def test_binary_primitive():
-
-    binary_primitive = ("and", lambda x,y: x & y)
-    width = 4;
-    input0 = range(16)
-    input1 = range(16)
-    context = Context()
-    primitive_name, primitive_op = binary_primitive
-    coreir_primitive = context.import_generator("coreir", primitive_name)
-    primitive16 = coreir_primitive(width=width)
-    module_typ = context.Record({
-        "in0": context.Array(width, context.BitIn()),
-        "in1": context.Array(width, context.BitIn()),
-        "out": context.Array(width, context.Bit())
-    })
-    module = context.global_namespace.new_module(f"test_{primitive_name}", module_typ)
-    module_def = module.new_definition()
-    primitive16_inst = module_def.add_module_instance(f"{primitive_name}_inst", primitive16)
-    interface = module_def.interface
-    module_def.connect(interface.select("in0"), primitive16_inst.select("in0"))
-    module_def.connect(interface.select("in1"), primitive16_inst.select("in1"))
-    module_def.connect(interface.select("out"), primitive16_inst.select("out"))
-    module.definition = module_def
-    sim_primitive16 = coreir.SimulatorState(module)
-
-    a = bit_vector.BitVector(16, 16)
-    b = bit_vector.BitVector(16, 16)
-    sim_primitive16.set_value(["self.in0"], a.as_bool_list())
-    sim_primitive16.set_value(["self.in1"], b.as_bool_list())
-    sim_primitive16.execute()
-    #assert BitVector(sim_primitive16.get_value(["self"], ["out"])) == primitive_op(a, b)
-
-
 def test_up_parallel():
     width = 5
     numOut = 2
