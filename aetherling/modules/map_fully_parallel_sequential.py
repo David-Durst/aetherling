b8cb9f176feccd4067cbdc2730d84188a60fa392
David Durst <davidbdurst@gmail.com>
2018-05-20 23:10:06 -0700

    made map have both define and instance calls for sequential and parallel, start of sdf implementation

--- aetherling/modules/map_fully_parallel_sequential.py
+++ aetherling/modules/map_fully_parallel_sequential.py
@@ -1,12 +1,22 @@
 from magma import *
 from magma.backend.coreir_ import CoreIRBackend
-from magma.frontend.coreir_ import CircuitInstanceFromGeneratorWrapper, GetCoreIRModule
+from magma.frontend.coreir_ import DefineCircuitFromGeneratorWrapper, GetCoreIRModule
 from ..helpers.nameCleanup import cleanName
 
-def MapParallel(cirb: CoreIRBackend, numInputs: int, op: Circuit) -> Circuit:
+def DefineSDFMapParallel(cirb: CoreIRBackend, numInputs: int, op: DefineCircuitKind) -> DefineCircuitKind:
+    """
+    Creates a MapParallel circuit defintion wrapped in an SDF
+    :param cirb:
+    :param numInputs:
+    :param op:
+    :return:
+    """
+
+def DefineMapParallel(cirb: CoreIRBackend, numInputs: int, op: DefineCircuitKind) -> DefineCircuitKind:
     """
     Map an operation over numInputs inputs in one clock cycle
     Aetherling Type: {1, T[numInputs]} -> {1, S[numInputs]}
+    This returns a circuit definition.
 
     :param cirb: The CoreIR backend currently be used
     :param numInputs: The number of input elements
@@ -20,21 +30,38 @@ def MapParallel(cirb: CoreIRBackend, numInputs: int, op: Circuit) -> Circuit:
     if op.is_instance and op.defn.instances.__contains__(op):
         op.defn.instances.remove(op)
     name = "MapParallel_n{}_op{}".format(str(numInputs), cleanName(str(type(op))))
-    moduleToReturn = CircuitInstanceFromGeneratorWrapper(cirb, "aetherlinglib", "mapParallel",
+    definitionToReturn = DefineCircuitFromGeneratorWrapper(cirb, "aetherlinglib", "mapParallel",
                                                          ["commonlib", "mantle", "coreir", "global"],
                                                          name,
                                                          {"numInputs": numInputs,
                                                           "operator": GetCoreIRModule(cirb, op)})
-    return moduleToReturn
+    return definitionToReturn
 
+def MapParallel(cirb: CoreIRBackend, numInputs: int, op: DefineCircuitKind) -> Circuit:
+    """
+    Map an operation over numInputs inputs in one clock cycle
+    Aetherling Type: {1, T[numInputs]} -> {1, S[numInputs]}
+    This returns a circuit instance, an instance of a circuit definition
 
-def MapSequential(cirb: CoreIRBackend, numInputs: int, op: Circuit) -> Circuit:
+    :param cirb: The CoreIR backend currently be used
+    :param numInputs: The number of input elements
+    :param op: The operator (the magma circuit) to map over the elements. It should have type T -> S
+    :return: A module with all the ports of op, except that they will be converted to arrays
+     of original type, with each array of length numInputs. For example, if op has
+     one input port I and one output O, the ports will be:
+    I : In(Array(numInputs, T))
+    O : Out(Array(numInputs, S))
+    """
+    return DefineMapParallel(cirb, numInputs, op)()
+
+def DefineMapSequential(cirb: CoreIRBackend, numInputs: int, op: DefineCircuitKind) -> DefineCircuitKind:
     """
     Map an operation over numInputs inputs over numInputs cycles.
     Note: the entire inputs must be delivered on the first cycle.
     There is no point in implementing the streaming version of this operation
     as that is just the module op.
     Aetherling Type: {1, T[numInputs]} -> {1, S[numInputs]}
+    This returns a circuit definition.
 
     :param cirb: The CoreIR backend currently be used
     :param numInputs: The number of input elements
@@ -49,10 +76,32 @@ def MapSequential(cirb: CoreIRBackend, numInputs: int, op: Circuit) -> Circuit:
     if op.is_instance and op.defn.instances.__contains__(op):
         op.defn.instances.remove(op)
     name = "MapSequential_n{}_op{}".format(str(numInputs), cleanName(str(type(op))))
-    moduleToReturn = CircuitInstanceFromGeneratorWrapper(cirb, "aetherlinglib", "mapSequential",
+    definitionToReturn = DefineCircuitFromGeneratorWrapper(cirb, "aetherlinglib", "mapSequential",
                                                          ["commonlib", "mantle", "coreir", "global"],
                                                          name,
                                                          {"numInputs": numInputs,
                                                           "operator": GetCoreIRModule(cirb, op)})
-    return moduleToReturn
+    return definitionToReturn
+
+
+def MapSequential(cirb: CoreIRBackend, numInputs: int, op: DefineCircuitKind) -> Circuit:
+    """
+    Map an operation over numInputs inputs over numInputs cycles.
+    Note: the entire inputs must be delivered on the first cycle.
+    There is no point in implementing the streaming version of this operation
+    as that is just the module op.
+    Aetherling Type: {1, T[numInputs]} -> {1, S[numInputs]}
+    This returns a circuit instance, an instance of a circuit definition
+
+    :param cirb: The CoreIR backend currently be used
+    :param numInputs: The number of input elements
+    :param op: The operator (the coreir module) to map over the elements. It should
+    s.t. Op : T -> S
+    :return: A module with all the ports of op, except that they will be converted to arrays
+     of original type, with each array of length numInputs. For example, if op has
+     one input port I and one output O, the ports will be:
+     I : In(Array(numInputs, T)
+     O : Out(Array(numInputs, S))
+    """
+    return DefineMapSequential(cirb, numInputs, op)()
 
