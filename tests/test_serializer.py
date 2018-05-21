0e46a82b94d70162669a0011d36bc6f86d9a35f1
David Durst <davidbdurst@gmail.com>
2018-05-21 16:24:06 -0700

    added deserializer test

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -17,13 +17,14 @@ def test_serializer():
     scope = Scope()
     inType = In(Array(numIn, Array(width, BitIn)))
     outType = Out(Array(width, Bit))
-    args = ['I', inType, 'O', outType] + ClockInterface(False, False)
+    args = ['I', inType, 'O', outType, 'ready', Out(Bit)] + ClockInterface(False, False)
 
     testcircuit = DefineCircuit('Test_Serializer', *args)
 
     serializer = Serializer(cirb, inType.T, numIn)
     wire(serializer.I, testcircuit.I)
     wire(testcircuit.O, serializer.out)
+    wire(testcircuit.ready, serializer.ready)
 
     EndCircuit()
 
@@ -51,9 +52,39 @@ def test_serializer():
         sim.set_value(testcircuit.I[i], int2seq(i, width), scope)
 
     for i in range(numIn):
-        assert sim.get_value(serializer.ready, scope) == (i == 0)
+        assert sim.get_value(testcircuit.ready, scope) == (i == 0)
         assert seq2int(sim.get_value(testcircuit.O, scope)) == i
         sim.evaluate()
         sim.advance_cycle()
         sim.evaluate()
-    assert sim.get_value(serializer.ready, scope) == True
+    assert sim.get_value(testcircuit.ready, scope) == True
+
+def test_deserializer():
+    width = 11
+    numIn = 13
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+    scope = Scope()
+    inType = In(Array(width, Bit))
+    outType = Out(Array(numIn, Array(width, BitIn)))
+    args = ['I', inType, 'O', outType, 'valid', Out(Bit)] + ClockInterface(False, False)
+
+    testcircuit = DefineCircuit('Test_Deserializer', *args)
+
+    deserializer = Deserializer(cirb, inType.T, numIn)
+    wire(deserializer.I, testcircuit.I)
+    wire(testcircuit.O, deserializer.out)
+    wire(testcircuit.valid, deserializer.valid)
+
+    EndCircuit()
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=cirb.context,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    for i in range(numIn):
+        sim.set_value(testcircuit.I, int2seq(i, width), scope)
+        assert sim.get_value(testcircuit.valid, scope) == (i == numIn - 1)
+        sim.evaluate()
+        sim.advance_cycle()
+        sim.evaluate()
+    assert sim.get_value(testcircuit.valid, scope) == False
