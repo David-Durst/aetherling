96293d0d8cbbe375dd73f3ac49ad5777c31cc77e
David Durst <davidbdurst@gmail.com>
2018-06-05 10:35:00 -0700

    added space time compose helpers, removed dead code, beginning compose for multiple firings

--- theory/SpaceTime/STCompute.hs
+++ theory/SpaceTime/STCompute.hs
@@ -25,6 +25,10 @@ class Composable a where
   -- This is for making ComposePar
   (|&|) :: Maybe a -> a -> Maybe a
 
+-- This is in same spirit as Monad's >>=, kinda abusing notation
+-- It's |.| in reverse so that can create pipelines in right order
+(|>>=|) op1 op0 = op0 (|.|) op1
+
 canComposeSeq :: (SpaceTime a) => a -> a -> Bool
 -- only join two nodes if token types match, ports do same number of tokens
 -- over all firings and streams per firing, and if same number of clock cycles
@@ -38,10 +42,17 @@ canComposePar :: (SpaceTime a) => a -> a -> Bool
 -- only join two nodes in parallel if same number of clocks
 canComposePar op0 op1 = (seqTime . time) op0 == (seqTime . time) op1
 
+-- space time helpers for compose that are used for all implementations
+spaceComposeSeq :: (SpaceTime a) => [a] -> Int
+spaceComposeSeq ops = foldl (|+|) addId $ map space ops
+spaceComposePar :: (SpaceTime a) => [a] -> Int
+spaceComposePar ops = foldl (|+|) addId $ map space ops
 
--- This is in same spirit as Monad's >>=, kinda abusing notation
--- It's |.| in reverse so that can create pipelines in right order
-(|>>=|) op1 op0 = op0 (|.|) op1
+timeComposeSeq :: (SpaceTime a) => [a] -> Int
+timeComposeSeq ops = foldl (|+|) addId $ map time ops
+timeComposePar :: (SpaceTime a) => [a] -> Int
+timeComposePar ops = SCTime (seqTime $ time $ head ops) maxCombTime
+    where maxCombTime = maximum $ map (combTime . time) ops
 
 -- These are leaf nodes for Op that do math
 data ArithmeticOp =
@@ -120,6 +131,7 @@ data SingleFiringOp =
   | ComposeParSF [SingleFiringOp]
   | ComposeSeqSF [SingleFiringOp]
   deriving (Eq, Show)
+
 
 instance SpaceTime SingleFiringOp where
   -- area of parallel map is area of all the copies
@@ -202,31 +214,17 @@ instance Composable SingleFiringOp where
   (|&|) (Just op0) op1 | canComposePar op0 op1 = Just $ ComposeParSF [op0, op1]
   (|&|) _ _ = Nothing
 
-
-canComposeSeq :: (SpaceTime a) => a -> a -> Bool
--- only join two nodes if token types match, ports do same number of tokens
--- over all firings and streams per firing, and if same number of clock cycles
-canComposeSeq op0 op1 = 
-  -- this checks both token types and numTokens over all firing/stream combos
-  portsScaledByFirings op0 == portsScaledByFirings op1 &&
-  (seqTime . time) op0 == (seqTime . time) op1
-  where portsScaledByFirings op = increasePortsStreamLens (numFirings op) $ outPortsType op
-
-canComposePar :: (SpaceTime a) => a -> a -> Bool
--- Iter handles mapping in multiple firing dimension
--- min length is 0 and there is no max
-data IterParams = IterParams { numIterations :: Int } deriving (Eq, Show)
-
-data MultipleFiringOp = Iter IterParams (Either MultipleFiringOp SingleFiringOp)
+data MultipleFiringOp = 
+  -- Int here is numIterations, min is 1 and no max
+  Iter Int SingleFiringOp
+  | ComposeParMF [MultipleFiringOp]
+  | ComposeSeqMF [MultipleFiringOp]
   deriving (Eq, Show)
 
 instance SpaceTime MultipleFiringOp where
   -- when mapping over sequence, area is time to count over sequence plus 
   -- area of stuff that is being applied to each element of sequence
-  space (Iter (IterParams numIters) (Left op)) = 
-    (counterSpace numIters) |+| (space op)
-  space (Iter (IterParams numIters) (Right op)) = 
-    (counterSpace numIters) |+| (space op)
+  space (Iter numIters op) = (counterSpace numIters) |+| (space op)
 
   time (Iter (IterParams numIters) (Left op)) =
     replicateTimeOverStream (time op) numIters
@@ -243,13 +241,6 @@ instance SpaceTime MultipleFiringOp where
 
   numFirings (Iter (IterParams n) (Left op)) = n * (numFirings op)
   numFirings (Iter (IterParams n) (Right op)) = n * (numFirings op)
-
--- inNumTokens :: (SpaceTime a) => a -> Int
--- inNumTokens op = tokensInOneFiring * n
---   where (IOSLens tokensInOneFiring _ n) = streamLens op
--- outNumTokens :: (SpaceTime a) => a -> Int
--- outNumTokens op = tokensOutOneFiring * n
---   where (IOSLens _ tokensOutOneFiring n) = streamLens op
 
 data Schedule1D = S_1D [MultipleFiringOp]
 
