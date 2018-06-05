afbdbc36fcd98114328c490e4673453a64ededfd
David Durst <davidbdurst@gmail.com>
2018-06-05 14:18:13 -0700

    split STHelpers into STTypes and STMetrics, split STCompute into STOpClasses and STOps

--- theory/SpaceTime/STMetrics.hs
+++ theory/SpaceTime/STMetrics.hs
@@ -1,4 +1,6 @@
-module SpaceTime.STHelpers where
+module SpaceTime.STMetrics where
+import SpaceTime.STTypes
+
 -- helpful functions and constants
 -- constant use for scaling operations in space and time
 rwTime :: Int
@@ -21,49 +23,6 @@ class MergeOrScale a where
   (|+|) :: a -> a -> a
   (|*) :: a -> Int -> a
   (|/) :: a -> Int -> a
-
-class HasLen a where
-  len :: a -> Int
-
--- the types used for tracking data about operators
-data TokenType =
-  T_Unit
-  | T_Int
-  | T_Bit
-  deriving (Eq, Show)
-
-instance HasLen TokenType where
-  len T_Unit = 0
-  len T_Int = 8
-  len T_Bit = 1
-
--- Int here is the length
-data TokensType = T_Array TokenType Int deriving (Eq, Show)
-
-instance HasLen TokensType where
-  len (T_Array t i) = i * len t
-
--- implicitly not banning multiple ports with same name here
--- names are only helpful reminders, can have duplicates with non-renamed ports
-data PortType = T_Port {pName :: [Char], pStreamLen :: Int, pTType :: TokensType}
-
-instance Eq PortType where
-  -- ignore names for equality, just check that all same
-  (==) (T_Port _ len0 tType0) (T_Port _ len1 tType1) = 
-    len0 == len1 && tType0 == tType1
-  (/=) pt0 pt1 = not $ pt0 == pt1
-
-duplicatePorts :: Int -> [PortType] ->  [PortType]
-duplicatePorts n ports = concat $ replicate n ports
-
-scalePortsStreamLens :: Int -> [PortType] -> [PortType]
-scalePortsStreamLens n ports = map mulPortStreamLen ports 
-  where mulPortStreamLen (T_Port pName pSLen tType) = T_Port pName (n*pSLen) tType
-
--- first int is stream length, second is array length
-portsFromTokens :: [([Char], Int, Int, TokenType)] -> [PortType]
-portsFromTokens ts = map makeTokens ts
-  where makeTokens (n, sLen, tNum, tType) = T_Port n sLen (T_Array tType tNum)
 
 -- first int tracks number of ops, second int tracks wiring consumed
 data OpsWireArea = OWA {opsArea :: Int, wireArea :: Int} deriving (Eq, Show)
---
+++ theory/SpaceTime/STTypes.hs
@@ -0,0 +1,45 @@
+module SpaceTime.STTypes where
+
+class HasLen a where
+  len :: a -> Int
+
+-- the types used for tracking data about operators
+data TokenType =
+  T_Unit
+  | T_Int
+  | T_Bit
+  deriving (Eq, Show)
+
+instance HasLen TokenType where
+  len T_Unit = 0
+  len T_Int = 8
+  len T_Bit = 1
+
+-- Int here is the length
+data TokensType = T_Array TokenType Int deriving (Eq, Show)
+
+instance HasLen TokensType where
+  len (T_Array t i) = i * len t
+
+-- implicitly not banning multiple ports with same name here
+-- names are only helpful reminders, can have duplicates with non-renamed ports
+data PortType = T_Port {pName :: [Char], pStreamLen :: Int, pTType :: TokensType}
+
+instance Eq PortType where
+  -- ignore names for equality, just check that all same
+  (==) (T_Port _ len0 tType0) (T_Port _ len1 tType1) = 
+    len0 == len1 && tType0 == tType1
+  (/=) pt0 pt1 = not $ pt0 == pt1
+
+duplicatePorts :: Int -> [PortType] ->  [PortType]
+duplicatePorts n ports = concat $ replicate n ports
+
+scalePortsStreamLens :: Int -> [PortType] -> [PortType]
+scalePortsStreamLens n ports = map mulPortStreamLen ports 
+  where mulPortStreamLen (T_Port pName pSLen tType) = T_Port pName (n*pSLen) tType
+
+-- first int is stream length, second is array length
+portsFromTokens :: [([Char], Int, Int, TokenType)] -> [PortType]
+portsFromTokens ts = map makeTokens ts
+  where makeTokens (n, sLen, tNum, tType) = T_Port n sLen (T_Array tType tNum)
+
