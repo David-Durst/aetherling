d52406e493fae2cb6a0374c1bb6602bdb3a7e11a
David Durst <davidbdurst@gmail.com>
2018-06-07 11:14:55 -0700

    calling |>>=| instead of >>= for functors, passing right number of arguments to linebuffer in conv example

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -7,15 +7,15 @@ import SpaceTime.STOps
 conv1PxPerClock = 
   (
     (
-      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Read $ T_Array 3 T_Int) >>=
+      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Read $ T_Array 3 T_Int) |>>=|
       Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
-        (1, T_Array 3 T_Int) (1, T_Array 1 (T_Array 3 T_Int))) >>=
-      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ LineBuffer (T_Array 3 T_Int)) >>=
+        (1, T_Array 3 T_Int) (1, T_Array 1 (T_Array 3 T_Int))) |>>=|
+      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ LineBuffer 1 3 (T_Array 3 T_Int)) |>>=|
       Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
         (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
     ) |&|
     Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Constant_Int 1 [1 1 1]) 
-  ) >>=
-  Just (ComposeContainerSF $ UtilHigherOrder 0 $ MapOp 3 3 $ LeafOp $ Add T_Int) >>=
-  Just (ComposeContainerSF $ UtilHigherOrder 0 $ ReduceOp 3 3 $ LeafOp $ Add T_Int) >>=
+  ) |>>=|
+  Just (ComposeContainerSF $ UtilHigherOrder 0 $ MapOp 3 3 $ LeafOp $ Add T_Int) |>>=|
+  Just (ComposeContainerSF $ UtilHigherOrder 0 $ ReduceOp 3 3 $ LeafOp $ Add T_Int) |>>=|
   Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Write T_Int)
