fbc9f5261186e322b6bd6ff950bcb6316e76ef8c
David Durst <davidbdurst@gmail.com>
2018-06-07 11:16:31 -0700

    fixing commas in array of example, builds but rates not matching up so get nothing for pipeline

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -14,7 +14,7 @@ conv1PxPerClock =
       Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
         (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
     ) |&|
-    Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Constant_Int 1 [1 1 1]) 
+    Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Constant_Int 1 [1, 1, 1]) 
   ) |>>=|
   Just (ComposeContainerSF $ UtilHigherOrder 0 $ MapOp 3 3 $ LeafOp $ Add T_Int) |>>=|
   Just (ComposeContainerSF $ UtilHigherOrder 0 $ ReduceOp 3 3 $ LeafOp $ Add T_Int) |>>=|
