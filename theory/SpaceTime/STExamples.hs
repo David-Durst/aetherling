348f5730bcbc59115b3b9c463f99fa7510638543
David Durst <davidbdurst@gmail.com>
2018-06-07 11:41:47 -0700

    slowing down const, adding back justs as remebed fmap allows me to deal with them

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -4,23 +4,23 @@ import SpaceTime.STMetrics
 import SpaceTime.STOpClasses
 import SpaceTime.STOps
 
-exMemRead = ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Read $ T_Array 3 T_Int
-exPart = ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
-  (1, T_Array 3 T_Int) (1, T_Array 1 (T_Array 3 T_Int))
-exLB = ComposeContainerSF $ UtilNonMapLeaf 0 $ LineBuffer 1 3 (T_Array 3 T_Int)
-exFlat = ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
-  (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int)
-exConst = ComposeContainerSF $ UtilNonMapLeaf 0 $ Constant_Int 1 [1, 1, 1]
+exMemRead = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Read $ T_Array 3 T_Int)
+exPart = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
+  (1, T_Array 3 T_Int) (1, T_Array 1 (T_Array 3 T_Int)))
+exLB = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ LineBuffer 1 3 (T_Array 3 T_Int))
+exFlat = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
+  (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
+exConst = Just (ComposeContainerSF $ UtilNonMapLeaf 2 $ Constant_Int 1 [1, 1, 1])
 
 conv1PxPerClock = 
   (
     (
-      Just exMemRead |>>=|
-      Just exPart |>>=|
-      Just exLB |>>=|
-      Just exFlat
+      exMemRead |>>=|
+      exPart |>>=|
+      exLB |>>=|
+      exFlat
     ) |&|
-    Just exConst
+    exConst
   ) |>>=|
   Just (ComposeContainerSF $ UtilHigherOrder 0 $ MapOp 3 3 $ LeafOp $ Add T_Int) |>>=|
   Just (ComposeContainerSF $ UtilHigherOrder 0 $ ReduceOp 3 3 $ LeafOp $ Add T_Int) |>>=|
