72b6a9ff138417948e88a9710d2be201b3a70966
David Durst <davidbdurst@gmail.com>
2018-06-07 11:19:35 -0700

    broke up conv example somewhat for debugging

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -4,17 +4,23 @@ import SpaceTime.STMetrics
 import SpaceTime.STOpClasses
 import SpaceTime.STOps
 
+exMemRead = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Read $ T_Array 3 T_Int)
+exPart = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
+  (1, T_Array 3 T_Int) (1, T_Array 1 (T_Array 3 T_Int)))
+exLB = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ LineBuffer 1 3 (T_Array 3 T_Int))
+exFlat = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
+  (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
+exConst = Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Constant_Int 1 [1, 1, 1]) 
+
 conv1PxPerClock = 
   (
     (
-      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Mem_Read $ T_Array 3 T_Int) |>>=|
-      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
-        (1, T_Array 3 T_Int) (1, T_Array 1 (T_Array 3 T_Int))) |>>=|
-      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ LineBuffer 1 3 (T_Array 3 T_Int)) |>>=|
-      Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ StreamArrayController
-        (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
+      exMemRead |>>=|
+      exPart |>>=|
+      exLB |>>=|
+      exFlat
     ) |&|
-    Just (ComposeContainerSF $ UtilNonMapLeaf 0 $ Constant_Int 1 [1, 1, 1]) 
+    exConst
   ) |>>=|
   Just (ComposeContainerSF $ UtilHigherOrder 0 $ MapOp 3 3 $ LeafOp $ Add T_Int) |>>=|
   Just (ComposeContainerSF $ UtilHigherOrder 0 $ ReduceOp 3 3 $ LeafOp $ Add T_Int) |>>=|
