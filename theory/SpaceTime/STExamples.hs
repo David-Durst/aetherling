6ee217c3457e4a3aa79b62703cdde800d8c3d6ba
David Durst <davidbdurst@gmail.com>
2018-06-12 11:55:01 -0700

    StExamples compiles with merged util and iter and pipelineTime data structure

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -4,20 +4,20 @@ import SpaceTime.STMetrics
 import SpaceTime.STOpClasses
 import SpaceTime.STOps
 
-exMemRead = Just (fullUtilSFToIter 1 $ SFLeafOp $ MemRead 10 $ T_Int)
-exPart = Just (ComposeContainer $ UtilOp 0 $ SFLeafOp $ 
+exMemRead = Just (fullIterSF 1 1 $ SFLeafOp $ MemRead 10 $ T_Int)
+exPart = Just (ComposeContainer $ IterOp 1 1 $ SFLeafOp $ 
   StreamArrayController (1, T_Int) (1, T_Array 1 T_Int))
-exLB = Just (ComposeContainer $ UtilOp 0 $ SFLeafOp $ LineBuffer 1 3 T_Int)
-exFlat = Just (ComposeContainer $ UtilOp 0 $ SFLeafOp $ 
+exLB = Just (ComposeContainer $ IterOp 1 1 $ SFLeafOp $ LineBuffer 1 3 T_Int)
+exFlat = Just (ComposeContainer $ IterOp 1 1 $ SFLeafOp $ 
   StreamArrayController (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
-exConst = Just (ComposeContainer $ IterOp 10 $ ComposeContainer $ UtilOp 1 $
+exConst = Just (ComposeContainer $ IterOp 10 10 $ ComposeContainer $ IterOp 1 1 $
   SFLeafOp $ Constant_Int 1 [1, 1, 1])
 
 conv1PxPerClock = 
   (
     (
       exMemRead |>>=|
-      fmap (ComposeContainer . IterOp 10) (
+      fmap (ComposeContainer . IterOp 10 10) (
         exPart |>>=|
         exLB |>>=|
         exFlat
@@ -25,8 +25,8 @@ conv1PxPerClock =
     ) |&|
     exConst
   ) |>>=|
-  fmap (ComposeContainer . IterOp 10) (
-    Just (ComposeContainer $ UtilOp 0 $ MapOp 3 3 $ SFLeafOp $ Add T_Int) |>>=|
-    Just (ComposeContainer $ UtilOp 0 $ ReduceOp 3 3 $ SFLeafOp $ Add T_Int)
+  fmap (ComposeContainer . IterOp 10 10) (
+    Just (ComposeContainer $ IterOp 1 1 $ MapOp 3 3 $ SFLeafOp $ Add T_Int) |>>=|
+    Just (ComposeContainer $ IterOp 1 1 $ ReduceOp 3 3 $ SFLeafOp $ Add T_Int)
   ) |>>=|
-  Just (fullUtilSFToIter 1 $ SFLeafOp $ MemWrite 10 $ T_Int)
+  Just (fullIterSF 1 1 $ SFLeafOp $ MemWrite 10 $ T_Int)
