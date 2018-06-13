a117bc7ac870d06af953e95a9f0c4a237ab2f77f
David Durst <davidbdurst@gmail.com>
2018-06-12 17:27:01 -0700

    cleaning up from iterops that are no longer necessary

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -5,10 +5,10 @@ import SpaceTime.STOpClasses
 import SpaceTime.STOps
 
 exMemRead = Just (fullIterSF 1 1 $ SFLeafOp $ MemRead 10 $ T_Int)
-exPart = Just (ComposeContainer $ IterOp 1 1 $ SFLeafOp $ 
+exPart = Just (ComposeContainer $ SFLeafOp $ 
   StreamArrayController (1, T_Int) (1, T_Array 1 T_Int))
-exLB = Just (ComposeContainer $ IterOp 1 1 $ SFLeafOp $ LineBuffer 1 3 T_Int)
-exFlat = Just (ComposeContainer $ IterOp 1 1 $ SFLeafOp $ 
+exLB = Just (ComposeContainer $ SFLeafOp $ LineBuffer 1 3 T_Int)
+exFlat = Just (ComposeContainer $ SFLeafOp $ 
   StreamArrayController (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
 exConst = Just (ComposeContainer $ RegDelay 3 0 $ IterOp 13 10 $
     SFLeafOp $ Constant_Int 1 [1, 1, 1])
@@ -26,7 +26,7 @@ conv1PxPerClock =
     exConst
   ) |>>=|
   fmap (ComposeContainer . IterOp 10 10) (
-    Just (ComposeContainer $ IterOp 1 1 $ MapOp 3 3 $ SFLeafOp $ Add T_Int) |>>=|
-    Just (ComposeContainer $ IterOp 1 1 $ ReduceOp 3 3 $ SFLeafOp $ Add T_Int)
+    Just (ComposeContainer $ MapOp 3 3 $ SFLeafOp $ Add T_Int) |>>=|
+    Just (ComposeContainer $ ReduceOp 3 3 $ SFLeafOp $ Add T_Int)
   ) |>>=|
   Just (fullIterSF 1 1 $ SFLeafOp $ MemWrite 10 $ T_Int)
