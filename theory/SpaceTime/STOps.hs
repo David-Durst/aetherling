a4529970333f77a597e5421c730d8d87aee11d75
David Durst <davidbdurst@gmail.com>
2018-06-13 22:27:23 -0700

    merged everythign except compose

--- theory/SpaceTime/STOps.hs
+++ theory/SpaceTime/STOps.hs
@@ -6,7 +6,8 @@ import SpaceTime.STOpClasses
 import Data.Typeable
 
 -- These are leaf nodes that can be used in a higher order operator
-data LeafOp =
+data Op =
+  -- LEAF OPS
   Add TokenType
   | Sub TokenType
   | Mul TokenType
@@ -16,11 +17,23 @@ data LeafOp =
   -- first Int is pixels per clock, second is window width, third int is 
   | LineBuffer {pxPerClock :: Int, windowWidth :: Int, lbInT :: TokenType}
   -- Array is constant produced, int is stream length
-  | Constant_Int Int [Int]
+  | Constant_Int {intConstStreamLen :: Int, intConstProduced :: [Int]}
   -- Array is constant produced, int is stream length
-  | Constant_Bit Int [Bool]
+  | Constant_Bit {bitConstStreamLen :: Int, bitConstProduced [Bool]}
   -- first pair is input stream length and tokens per stream element, second is output
   | StreamArrayController (Int, TokenType) (Int, TokenType)
+
+  -- HIGHER ORDER OPS
+  | MapOp {mapParallelism :: Int, mapStreamLen :: Int, mappedOp :: Op}
+  | ReduceOp {reduceParallelism :: Int, reduceStreamLen :: Int, reducedOp :: Op}
+
+  -- ITERATION AND UNDERUTILIZATION OPS
+  -- First Int is num iterations, second is num iterations active
+  | IterOp {numIterations :: Int, numIterationsActive :: Int, iteredOp :: Op}
+  -- delayStages is stages to add, delayClocks is clocks to add to each stage 
+  -- when doing nothing, a is for thing to wrap with registers
+  -- put registers after each stage of spacetime being wrapped
+  | RegDelay {delayStages :: Int, delayClocks :: Int, delayedOp :: Op}
   deriving (Eq, Show)
 
 twoInSimplePorts t = [T_Port "I0" 1 t, T_Port "I1" 1 t]
@@ -49,6 +62,23 @@ instance SpaceTime LeafOp where
   -- entire input
   space (StreamArrayController (inSLen, inType) _) = registerSpace [inType] |* inSLen
 
+  -- area of parallel map is area of all the copies
+  space (MapOp pEl _ op) = (space op) |* pEl
+  -- area of reduce is area of reduce tree, with area for register for partial
+  -- results and counter for tracking iteration time if input is stream of more
+  -- than what is passed in one clock
+  space (ReduceOp pEl totEl op) | pEl == totEl = (space op) |* (pEl - 1)
+  space rOp@(ReduceOp pEl _ op) =
+    reduceTreeSpace |+| (space op) |+| (registerSpace $ map pTType $ outPortsType op)
+    |+| (counterSpace $ seqTime $ time rOp)
+    where reduceTreeSpace = space (ReduceOp pEl pEl op)
+
+  space iOp@(IterOp numIters _ op) = space op |+| counterSpace (seqTime $ time iOp)
+  space (RegDelay ds dc op) = counterSpace dc |+| 
+    ((registerSpace $ map pTType $ inPortsType op) |* (ds + (numStages $ pipelineTime op))) |+| space op
+
+
+
   time (Add t) = SCTime 0 1
   time (Sub t) = SCTime 0 1
   time (Mul t) = SCTime 0 mulSpaceTimeIncreaser
@@ -63,12 +93,41 @@ instance SpaceTime LeafOp where
   time (StreamArrayController (inSLen, _) (outSLen, _)) = registerTime |* 
     lcm inSLen outSLen
 
+  time (MapOp pEl totEl op) = replicateTimeOverStream (totEl `ceilDiv` pEl) (time op)
+  time (ReduceOp pEl totEl op) | pEl == totEl = (time op) |* (ceilLog pEl)
+  time (ReduceOp pEl totEl op) =
+    replicateTimeOverStream (totEl `ceilDiv` pEl) (reduceTreeTime |+| (time op) |+| registerTime)
+    where reduceTreeTime = time (ReduceOp pEl pEl op)
+
+  time (IterOp numIters _ op) = replicateTimeOverStream numIters (time op)
+  time (RegDelay ds dc op) = time op |+| 
+    SCTime (dc * (ds + (numStages $ pipelineTime op))) 0
+
   -- what's correct here for streamarraycontroller for non-zero seqtime?
   -- for everythign else, emitting something every clock if sequential, and 
   -- no impact on clocks if combinational
   pipelineTime op = PTime 1 1
 
-  util _ = 1.0
+  util (Add t) = 1
+  util (Sub t) = 1
+  util (Mul t) = 1
+  util (Div t) = 1
+  util (MemRead sLen _) = 1
+  util (MemWrite sLen _) = 1
+  util (LineBuffer _ _ _) = 1
+  util (Constant_Int _ _) = 1
+  util (Constant_Bit _ _) = 1
+  util (StreamArrayController (inSLen, _) (outSLen, _)) | 
+
+  util (MapOp _ _ op) = util op
+  util (ReduceOp _ _ op) = util op
+
+  util (IterOp totalIters usedIters op) = util op * (fromIntegral usedIters) /
+    (fromIntegral $ totalIters)
+  -- ignoring dleay stages for now, need to come back to them later
+  -- I think no underutilziation here, as registers being used for pipelining
+  util (RegDelay _ dc op) = util op
+
 
   inPortsType (Add t) = twoInSimplePorts t
   inPortsType (Sub t) = twoInSimplePorts t
@@ -80,6 +139,17 @@ instance SpaceTime LeafOp where
   inPortsType (Constant_Int _ _) = []
   inPortsType (Constant_Bit _ _) = []
   inPortsType (StreamArrayController (inSLen, inType) _) = [T_Port "I" inSLen inType]
+
+  inPortsType (MapOp pEl totEl op) = duplicatePorts pEl $
+    scalePortsStreamLens (totEl `ceilDiv` pEl) (inPortsType op)
+  inPortsType (ReduceOp pEl totEl op) = duplicatePorts pEl $
+  -- can just take head as can only reduce binary operators
+  -- parallelism means apply binary to pEl at a time
+    scalePortsStreamLens (totEl `ceilDiv` pEl) [head $ inPortsType op]
+
+  inPortsType (IterOp _ usedIters op) = scalePortsStreamLens usedIters $ inPortsType op
+  inPortsType (RegDelay _ _ op) = inPortsType op
+
 
   outPortsType (Add t) = oneOutSimplePort t
   outPortsType (Sub t) = oneOutSimplePort t
@@ -93,117 +163,11 @@ instance SpaceTime LeafOp where
   outPortsType (Constant_Int n ints) = [T_Port "O" n (T_Array (length ints) T_Int)]
   outPortsType (Constant_Bit n bits) = [T_Port "O" n (T_Array (length bits) T_Bit)]
   outPortsType (StreamArrayController _ (outSLen, outType)) = [T_Port "O" outSLen outType]
-  
-  numFirings _ = 1
-  
-
-data SingleFiringOp = 
-  -- First Int is parallelism, second is total number elements reducing
-  forall a. (SpaceTime a, Eq a, Show a, Typeable a) => MapOp Int Int a
-  -- Should I use a GADT to verify op for reduce has 2 inputs only?
-  -- First Int is parallelism, second is total number elements reducing
-  -- can't reduce a mem_read
-  | forall a. (SpaceTime a, Eq a, Show a, Typeable a) => ReduceOp Int Int a
-
-deriving instance Show SingleFiringOp
-
-instance Eq SingleFiringOp where
-  (==) (MapOp pEl0 totEl0 op0) (MapOp pEl1 totEl1 op1) = case cast op1 of 
-    Nothing -> False 
-    Just op1T0 -> op0 == op1T0 && pEl0 == pEl1 && totEl0 == totEl1
-  (==) (ReduceOp pEl0 totEl0 op0) (ReduceOp pEl1 totEl1 op1) = case cast op1 of 
-    Nothing -> False 
-    Just op1T0 -> op0 == op1T0 && pEl0 == pEl1 && totEl0 == totEl1
-  (/=) op0 op1 = not (op0 /= op1)
-
-instance SpaceTime SingleFiringOp where
-  -- area of parallel map is area of all the copies
-  space (MapOp pEl _ op) = (space op) |* pEl
-  -- area of reduce is area of reduce tree, with area for register for partial
-  -- results and counter for tracking iteration time if input is stream of more
-  -- than what is passed in one clock
-  space (ReduceOp pEl totEl op) | pEl == totEl = (space op) |* (pEl - 1)
-  space rOp@(ReduceOp pEl _ op) =
-    reduceTreeSpace |+| (space op) |+| (registerSpace $ map pTType $ outPortsType op)
-    |+| (counterSpace $ seqTime $ time rOp)
-    where reduceTreeSpace = space (ReduceOp pEl pEl op)
-
-  time (MapOp pEl totEl op) = replicateTimeOverStream (totEl `ceilDiv` pEl) (time op)
-  time (ReduceOp pEl totEl op) | pEl == totEl = (time op) |* (ceilLog pEl)
-  time (ReduceOp pEl totEl op) =
-    replicateTimeOverStream (totEl `ceilDiv` pEl) (reduceTreeTime |+| (time op) |+| registerTime)
-    where reduceTreeTime = time (ReduceOp pEl pEl op)
-
-  -- can't pipeline through a sequential map or reduce
-  pipelineTime (MapOp pEl totEl op) | pEl == totEl = pipelineTime op
-  pipelineTime m@(MapOp _ _ op) = PTime 1 (seqTime $ time m)
-  pipelineTime (ReduceOp pEl totEl op) | pEl == totEl = (pipelineTime op) |* (ceilLog pEl)
-  pipelineTime r@(ReduceOp _ _ op) = PTime 1 (seqTime $ time r)
-
-  util (MapOp _ _ op) = util op
-  util (ReduceOp _ _ op) = util op
-
-  inPortsType (MapOp pEl totEl op) = duplicatePorts pEl $
-    scalePortsStreamLens (totEl `ceilDiv` pEl) (inPortsType op)
-  inPortsType (ReduceOp pEl totEl op) = duplicatePorts pEl $
-  -- can just take head as can only reduce binary operators
-  -- parallelism means apply binary to pEl at a time
-    scalePortsStreamLens (totEl `ceilDiv` pEl) [head $ inPortsType op]
 
   outPortsType (MapOp pEl totEl op) = duplicatePorts pEl $
     scalePortsStreamLens (totEl `ceilDiv` pEl) (outPortsType op)
   outPortsType (ReduceOp _ _ op) = outPortsType op
 
-  numFirings (MapOp _ _ op) = numFirings op
-  numFirings (ReduceOp _ _ op) = numFirings op
-
-data IterOp = 
-  -- First Int is num iterations, second is num iterations active
-  forall a. (SpaceTime a, Eq a, Show a, Typeable a) => IterOp Int Int a
-  -- delayStages is stages to add, delayClocks is clocks to add to each stage 
-  -- when doing nothing, a is for thing to wrap with registers
-  -- put registers after each stage of spacetime being wrapped
-  | forall a. (SpaceTime a, Eq a, Show a, Typeable a) =>
-    RegDelay {delayStages :: Int, delayClocks :: Int, wrappedModule :: a}
-
-deriving instance Show IterOp
-
-instance Eq IterOp where
-  (==) (IterOp ni0 ui0 op0) (IterOp ni1 ui1 op1) = case cast op1 of 
-    Nothing -> False 
-    Just op1T0 -> op0 == op1T0 && ni0 == ni1 && ui0 == ui1
-  (==) (RegDelay ds0 dc0 op0) (RegDelay ds1 dc1 op1) = case cast op1 of 
-    Nothing -> False 
-    Just op1T0 -> op0 == op1T0 && ds0 == ds1 && dc0 == dc1
-  (/=) op0 op1 = not (op0 /= op1)
-
-floatUsedClocks :: (SpaceTime a) => a -> Float
-floatUsedClocks = fromIntegral . seqTime . time
-
-instance SpaceTime IterOp where
-  space iOp@(IterOp numIters _ op) = space op |+| counterSpace (seqTime $ time iOp)
-  space (RegDelay ds dc op) = counterSpace dc |+| 
-    ((registerSpace $ map pTType $ inPortsType op) |* (ds + (numStages $ pipelineTime op))) |+| space op
-
-  time (IterOp numIters _ op) = replicateTimeOverStream numIters (time op)
-  time (RegDelay ds dc op) = time op |+| 
-    SCTime (dc * (ds + (numStages $ pipelineTime op))) 0
-
-  pipelineTime (IterOp numIters _ op) = pipelineTime op
-  pipelineTime (RegDelay ds dc op) = PTime (ds + (numStages $ pipelineTime op)) 
-    (dc + (numClocks $ pipelineTime op))
-
-  util (IterOp totalIters usedIters op) = util op * (fromIntegral usedIters) /
-    (fromIntegral $ totalIters)
-  -- ignoring dleay stages for now, need to come back to them later
-  -- I think no underutilziation here, as registers being used for pipelining
-  util (RegDelay _ dc op) = util op
-
-  inPortsType (IterOp _ usedIters op) = scalePortsStreamLens usedIters $ inPortsType op
-  inPortsType (RegDelay _ _ op) = inPortsType op
-
   outPortsType (IterOp _ usedIters op) = scalePortsStreamLens usedIters $ outPortsType op
   outPortsType (RegDelay _ _ op) = outPortsType op
 
-  numFirings (IterOp _ usedIters op) = usedIters * numFirings op
-  numFirings (RegDelay _ _ op) = numFirings op
