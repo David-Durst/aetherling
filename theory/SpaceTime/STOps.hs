5bb537d734cfd2dd55fd1266cb1bf8197eed61a9
David Durst <davidbdurst@gmail.com>
2018-06-18 00:39:54 -0700

    finished first draft of latency, cps, and making util complete in one file

--- theory/SpaceTime/STOps.hs
+++ theory/SpaceTime/STOps.hs
@@ -3,6 +3,7 @@ module SpaceTime.STOps where
 import SpaceTime.STTypes
 import SpaceTime.STMetrics
 import Data.Typeable
+import Data.Bool
 
 -- These are leaf nodes that can be used in a higher order operator
 data Op =
@@ -75,24 +76,32 @@ space (MapOp pEl _ op) = (space op) |* pEl
 space (ReduceOp pEl totEl op) | pEl == totEl = (space op) |* (pEl - 1)
 space rOp@(ReduceOp pEl _ op) =
   reduceTreeSpace |+| (space op) |+| (registerSpace $ map pTType $ outPortsType op)
-  |+| (counterSpace $ seqTime $ time rOp)
+  |+| (counterSpace $ cps rOp)
   where reduceTreeSpace = space (ReduceOp pEl pEl op)
 
-space iOp@(IterOp numIters _ op) = space op |+| counterSpace (seqTime $ time iOp)
-space (RegDelay ds dc op) = counterSpace dc |+| 
-  ((registerSpace $ map pTType $ inPortsType op) |* (ds + (numStages $ pipelineTime op))) |+| space op
+space (IterOp numIters op) = space op |+| counterSpace numIters
+space (Underutil denom op) = space op |+| counterSpace denom
+space (RegDelay dc op) = space op |+|
+  ((registerSpace $ map pTType $ outPortsType op) |* dc)
 
 space (ComposePar ops) = foldl (|+|) addId $ map space ops
 space (ComposeSeq ops) = foldl (|+|) addId $ map space ops
 space (ComposeFailure _ _) = 0
 
+-- scaleCPS depending on if Op is combinational or not
+scaleCPS :: Op -> Int -> Int
+scaleCPS op n | isCombinational = 1
+scaleCPS op n = cps op * n
+
 cps op = clocksPerStream op
 clocksPerStream :: Op -> Int
+registerCPS = 1
 
 clocksPerStream (Add t) = 1
 clocksPerStream (Sub t) = 1
 clocksPerStream (Mul t) = 1
 clocksPerStream (Div t) = 1
+-- to what degree can we pipeline MemRead and MemWrite
 clocksPerStream (MemRead _) = rwTime
 clocksPerStream (MemWrite _) = rwTime
 clocksPerStream (LineBuffer _ _ _) = 1
@@ -102,21 +111,69 @@ clocksPerStream (StreamArrayController (inSLen, _) (outSLen, _)) |
   inSLen == 1 && outSLen == 1 = 1
 clocksPerStream (StreamArrayController (inSLen, _) (outSLen, _)) = lcm inSLen outSLen
 
-clocksPerStream (MapOp pEl totEl op) = replicateTimeOverStream (totEl `ceilDiv` pEl) (time op)
-clocksPerStream (ReduceOp pEl totEl op) | pEl == totEl = (time op) |* (ceilLog pEl)
+clocksPerStream (MapOp pEl totEl op) = cps op * (totEl `ceilDiv` pEl)
+-- can only pipeline a reduce if its fully parallel
+clocksPerStream (ReduceOp pEl totEl op) | pEl `mod` totEl == 0 =
+  scaleCPS op (ceilLog totEl)
 clocksPerStream (ReduceOp pEl totEl op) =
-  replicateclocksPerStreamOverStream (totEl `ceilDiv` pEl) (reduceTreeTime |+| (time op) |+| registerTime)
-  where reduceTreeclocksPerStream = time (ReduceOp pEl pEl op)
+  (totEl `ceilDiv` pEl) * (reduceTreeCPS + cps op + registerCPS)
+  where 
+    reduceTreeCPS = time (ReduceOp pEl pEl op)
+    -- op adds nothing if its combinational, its CPS else
+    opCPS = bool 0 (cps op) (isCombinational op)
 
-clocksPerStream (IterOp numIters _ op) = replicateTimeOverStream numIters (time op)
-clocksPerStream (RegDelay ds dc op) = time op |+| 
-  SCclocksPerStream (dc * (ds + (numStages $ pipelineTime op))) 0
+clocksPerStream (IterOp numIters op) = numIters * clocksPerStream op
+clocksPerStream (Underutil denom op) = clocksPerStream op * denom
+-- since pipelined, this doesn't affect clocks per stream
+clocksPerStream (RegDelay _ op) = clocksPerStream op
 
--- what's correct here for streamarraycontroller for non-zero seqclocksPerStream?
--- for everythign else, emitting something every clock if sequential, and 
--- no impact on clocks if combinational
-pipelineTime op = PTime 1 1
+-- this depends the constructors verifying that only composing valid things
+-- see the document for what is valid
+clocksPerStream (ComposePar (hd:tl)) = cps hd
+clocksPerStream (ComposeSeq (hd:tl)) = cps hd
+clocksPerStream (ComposeFail@ure _ _) = 0
 
+
+latency :: a -> Int
+latency (Add t) = 1
+latency (Sub t) = 1
+latency (Mul t) = 1
+latency (Div t) = 1
+latency (MemRead _) = 1
+latency (MemWrite _) = 1
+-- for each extra element in per clock, first output is 1 larger, but get 1
+-- extra every clock building up to first output
+latency (LineBuffer p w _) = (w + p - 1) / p
+latency (Constant_Int _ _) = 1
+latency (Constant_Bit _ _) = 1
+latency (StreamArrayController (inSLen, _) (outSLen, _)) = 1
+
+latency (MapOp _ _ op) = latency op
+latency (ReduceOp pEl totEl op) | pEl `mod` totEl == 0 && isCombinational op = 1
+latency (ReduceOp pEl totEl op) | pEl `mod` totEl == 0 = latency op * (totEl pEl)
+latency (ReduceOp pEl totEl op) =
+  (totEl `ceilDiv` pEl) * (reduceTreeCPS + cps op + registerCPS)
+  where 
+    reduceTreeCPS = time (ReduceOp pEl pEl op)
+    -- op adds nothing if its combinational, its CPS else
+    opCPS = bool 0 (cps op) (isCombinational op)
+
+
+latency (IterOp numIters op) = latency op
+latency (Underutil denom op) = latency op
+-- since pipelined, this doesn't affect clocks per stream
+latency (RegDelay dc op) = latency op + dc
+
+latency (ComposePar ops) = maximum $ map latency ops
+-- latency is 1 if all elemetns are combintional, sum of latencies of sequential
+-- elements otherwise
+latency (ComposeSeq ops) = util ops
+  where 
+    combinatonalLatency = 1
+    sequentialLatency = foldl (+) 0 $ map latency $ filter (not . isCombinational) ops
+
+
+util :: a -> Float
 util (Add t) = 1
 util (Sub t) = 1
 util (Mul t) = 1
@@ -126,17 +183,25 @@ util (MemWrite _) = 1
 util (LineBuffer _ _ _) = 1
 util (Constant_Int _ _) = 1
 util (Constant_Bit _ _) = 1
-util (StreamArrayController (inSLen, _) (outSLen, _)) | 
+util (StreamArrayController (inSLen, _) (outSLen, _)) = 1
 
 util (MapOp _ _ op) = util op
 util (ReduceOp _ _ op) = util op
 
-util (IterOp totalIters usedIters op) = util op * (fromIntegral usedIters) /
-  (fromIntegral $ totalIters)
--- ignoring dleay stages for now, need to come back to them later
--- I think no underutilziation here, as registers being used for pipelining
-util (RegDelay _ dc op) = util op
+util (IterOp numIters op) = util op
+util (Underutil denom op) = util op / denom
+-- since pipelined, this doesn't affect clocks per stream
+util (RegDelay _ op) = util op
 
+util (ComposePar ops) = utilWeightedByArea ops
+util (ComposeSeq ops) = utilWeightedByArea ops
+-- is there a better utilization than weighted by operator area
+utilWeightedByArea :: (SpaceTime a) => [a] -> Float
+utilWeightedByArea ops = unnormalizedUtil / totalArea
+    where 
+      unnormalizedUtil = foldl (+) 0 $
+        map (\op -> (fromIntegral $ opsArea $ space op) * (util op)) ops
+      totalArea = foldl (+) 0 $ map (fromIntegral . opsArea . space) ops
 
 inPortsType (Add t) = twoInSimplePorts t
 inPortsType (Sub t) = twoInSimplePorts t
