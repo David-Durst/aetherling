0722da0fca99e2228367f6b402c434eca3f9d8d7
David Durst <davidbdurst@gmail.com>
2018-06-30 16:28:42 -0700

    1d conv example compiles

--- theory/SpaceTime/STExamples.hs
+++ theory/SpaceTime/STExamples.hs
@@ -11,23 +11,23 @@ conv1PxPerClock =
       MemRead T_Int |>>=|
       SequenceArrayController (1, T_Int) (1, T_Array 1 T_Int) |>>=|
       LineBuffer 1 3 T_Int |>>=|
-      SequenceArrayController (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int))
+      SequenceArrayController (1, T_Array 1 (T_Array 3 T_Int)) (1, T_Array 3 T_Int)
     ) |&|
-    Constant_Int 1 [1, 1, 1]
+    Constant_Int [1, 1, 1]
   ) |>>=|
-  MapOp 3 3 (Add T_Int) |>>=|
+  MapOp 3 (Add T_Int) |>>=|
   ReduceOp 3 3 (Add T_Int) |>>=|
   MemWrite T_Int
 
 describeMethod name op = do
   print name
-  print $ "In Ports: " ++ inPorts op
-  print $ "Out Ports: " ++ outPorts op
-  print $ "Clocks Per Sequence " ++ cps op
-  print $ "Area: " ++ space op
-  print $ "Initial Latency: " ++ initialLatency op
-  print $ "Maximum Combinational Path: " ++ maxCombPath op
-  print $ "Utilization: " ++ util op
+  print $ "In Ports: " ++ show (inPorts op)
+  print $ "Out Ports: " ++ show (outPorts op)
+  print $ "Clocks Per Sequence " ++ show (cps op)
+  print $ "Area: " ++ show (space op)
+  print $ "Initial Latency: " ++ show (initialLatency op)
+  print $ "Maximum Combinational Path: " ++ show (maxCombPath op)
+  print $ "Utilization: " ++ show (util op)
   print " "
 
 main = do
