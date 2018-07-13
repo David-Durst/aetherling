80699d4cac8b527b13b1542fb01c00d784ec4ab2
David Durst <davidbdurst@gmail.com>
2018-07-13 15:44:01 -0700

    no more - in cleaned up names

--- aetherling/helpers/nameCleanup.py
+++ aetherling/helpers/nameCleanup.py
@@ -1,2 +1,2 @@
 def cleanName(name: str):
-    return name.replace("(", "_").replace(")", "_").replace(",", "-").replace(" ", "").replace(":", "-")
+    return name.replace("(", "_").replace(")", "_").replace(",", "_").replace(" ", "").replace(":", "_").replace("-","_")
