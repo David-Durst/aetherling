0830e6c217686182014489365b48072ad02b7b42
David Durst <davidbdurst@gmail.com>
2018-09-12 14:43:35 -0700

    testing delayed buffer with parallel inputs

--- tests/test_delayed_buffer.py
+++ tests/test_delayed_buffer.py
@@ -9,15 +9,10 @@ from magma.scope import Scope
 from magma.bitutils import *
 
 
-def test_delayed_buffer():
-    width = 5
-    numOut = 2
-    testVal = 3
+def test_delayed_buffer_serial():
     c = coreir.Context()
     cirb = CoreIRBackend(c)
     scope = Scope()
-    inType = Array(width, In(BitIn))
-    outType = Out(inType)
     testcircuit = DefineDelayedBuffer(cirb, Array(8, Bit), 3, 1, 12)
 
     sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=cirb.context)
@@ -45,7 +40,38 @@ def test_delayed_buffer():
         sim.advance_cycle()
         sim.evaluate()
 
+def test_delayed_buffer_parallel():
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+    scope = Scope()
+    testcircuit = DefineDelayedBuffer(cirb, Array(8, Bit), 4, 2, 16)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=cirb.context)
+
+    sim.set_value(testcircuit.CE, True, scope)
+    last_output = 2
+    last_input = 2
+    # do two cycles
+    for clock_index in range(32):
+        # for each cycle, input every other of first half
+        if clock_index % 16 < 4 and clock_index % 2 == 0:
+            sim.set_value(testcircuit.WE, True, scope)
+            sim.set_value(testcircuit.I[0], int2seq(last_input, 8), scope)
+            sim.set_value(testcircuit.I[1], int2seq(last_input + 1, 8), scope)
+            last_input += 2
+        else:
+            sim.set_value(testcircuit.WE, False, scope)
+        sim.evaluate()
+
+        if clock_index % 4 == 0:
+            assert sim.get_value(testcircuit.valid, scope) == True
+            assert seq2int(sim.get_value(testcircuit.O, scope)[0]) == last_output
+            last_output += 1
+        else:
+            assert sim.get_value(testcircuit.valid, scope) == False
+        sim.advance_cycle()
+        sim.evaluate()
 
 
 if __name__ == "__main__":
-    test_delayed_buffer()
+    test_delayed_buffer_serial()
