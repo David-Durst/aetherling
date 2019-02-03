e4c0a60f77fcf81bab3a36698b0f7f1ab3318acd
David Durst <davidbdurst@gmail.com>
2019-02-03 14:13:16 -0800

    added epilogue and prologue for all generated aetherling python files from haskell

---
+++ aetherling/HaskellPrelude.py
@@ -0,0 +1,21 @@
+from aetherling.modules.reduce import DefineReduceSequential, DefineReduceParallel, renameCircuitForReduce
+from aetherling.modules.term_any_type import TermAnyType
+from magma.backend.coreir_ import CoreIRBackend
+from magma.bitutils import *
+from coreir.context import *
+from magma.simulator.coreir_simulator import CoreIRSimulator
+import coreir
+from magma.scope import Scope
+from mantle.coreir.arith import *
+from mantle.coreir.logic import *
+from mantle.coreir.compare import *
+from mantle.coreir import DefineCoreirConst
+from mantle.coreir.LUT import *
+from aetherling.modules.upsample import *
+from aetherling.modules.downsample import *
+from aetherling.modules.reduce import *
+from aetherling.modules.native_linebuffer.two_dimensional_native_linebuffer import DefineTwoDimensionalLineBuffer
+
+c = coreir.Context()
+cirb = CoreIRBackend(c)
+
