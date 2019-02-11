3e69ff4da1322e95728d26317601ec8304958b8c
David Durst <davidbdurst@gmail.com>
2019-02-10 23:26:06 -0800

    a bunch of helper tests that I don't want running every time

---
+++ tests/helper_test_readyvalid.py
@@ -0,0 +1,210 @@
+from aetherling.modules.noop import DefineNoop
+from magma import *
+from mantle.coreir import DefineCoreirConst
+from magma.clock import *
+from magma.backend.coreir_ import CoreIRBackend, compile
+from magma.bitutils import *
+from coreir.context import *
+from magma.simulator.coreir_simulator import CoreIRSimulator
+import coreir
+from magma.scope import Scope
+from mantle.common.countermod import CounterModM, Decode
+from aetherling.modules.native_linebuffer.two_dimensional_native_linebuffer import DefineTwoDimensionalLineBuffer
+from aetherling.modules.native_linebuffer.one_dimensional_native_linebuffer import DefineOneDimensionalLineBuffer
+from aetherling.modules.delayed_buffer import DefineDelayedBuffer
+from mantle.coreir.memory import DefineRAM, getRAMAddrWidth
+from aetherling.modules.ram_any_type import DefineRAMAnyType
+
+def test_2dlb_flicker_ce_with_2x2_stride():
+    scope = Scope()
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+
+    testcircuit = DefineTwoDimensionalLineBuffer(cirb, Array(8, In(Bit)), 1, 1, 2, 2, 8, 8, 2, 2, 0, 0, True)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=c,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    for i in range(100000):
+        if i % 2 == 0:
+            sim.set_value(testcircuit.I[0][0], int2seq(1, 8), scope)
+            sim.set_value(testcircuit.CE, 1, scope)
+        else:
+            sim.set_value(testcircuit.I[0][0], int2seq(2, 8), scope)
+            sim.set_value(testcircuit.CE, 0, scope)
+        sim.evaluate()
+        sim.advance_cycle()
+        sim.evaluate()
+        print("i" + str(i))
+        print("undelayed out: " + str([seq2int(sim.get_value(testcircuit.undelayedO[0][r][c], scope)) for r in range(2) for c in range(2)]))
+        print("out: " + str([seq2int(sim.get_value(testcircuit.O[0][r][c], scope)) for r in range(2) for c in range(2)]))
+        print("valid: " + str(sim.get_value(testcircuit.valid, scope)))
+        print("db CE: " + str(sim.get_value(testcircuit.dbCE, scope)))
+        print("db WE: " + str(sim.get_value(testcircuit.dbWE, scope)))
+        print("db RDATA: " + str([seq2int(sim.get_value(testcircuit.RDATA, scope)[0][r][c]) for r in range(2) for c in range(2)]))
+        print("db WDATA: " + str([seq2int(sim.get_value(testcircuit.WDATA, scope)[0][r][c]) for r in range(2) for c in range(2)]))
+        print("db RADDR: " + str(seq2int(sim.get_value(testcircuit.RADDR, scope)[0])))
+        print("db WADDR: " + str(seq2int(sim.get_value(testcircuit.WADDR, scope)[0])))
+        print("db RAM WE: " + str(sim.get_value(testcircuit.RAMWE, scope)))
+        print("")
+        print("")
+
+        # for some reason, lb going to 0 when flickering valid on and off for ce
+        for r in range(2):
+            for c in range(2):
+                assert (sim.get_value(testcircuit.valid, scope) == 0 or seq2int(sim.get_value(testcircuit.O[0][r][c], scope)) != 0)
+
+def test_1dlb_flicker_ce_with_2_stride():
+    scope = Scope()
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+
+    testcircuit = DefineOneDimensionalLineBuffer(cirb, Array(8, In(Bit)), 1, 2, 8, 2, 0, True)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=c,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    for i in range(50):
+        if i % 2 == 0:
+            sim.set_value(testcircuit.I[0], int2seq(1, 8), scope)
+            sim.set_value(testcircuit.CE, 1, scope)
+        else:
+            sim.set_value(testcircuit.I[0], int2seq(2, 8), scope)
+            sim.set_value(testcircuit.CE, 0, scope)
+        sim.evaluate()
+        sim.advance_cycle()
+        sim.evaluate()
+        print([seq2int(sim.get_value(testcircuit.O[0][r], scope)) for r in range(2)])
+        print(sim.get_value(testcircuit.valid, scope))
+        # for some reason, lb going to 0 when flickering valid on and off for ce
+        for r in range(2):
+            assert (sim.get_value(testcircuit.valid, scope) == 0 or seq2int(sim.get_value(testcircuit.O[0][r], scope)) != 0)
+
+def test_2dlb_flicker_ce_with_no_stride():
+    scope = Scope()
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+
+    testcircuit = DefineTwoDimensionalLineBuffer(cirb, Array(8, In(Bit)), 1, 1, 2, 2, 8, 8, 1, 1, 0, 0, True)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=c,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    for i in range(100000):
+        if i % 2 == 0:
+            sim.set_value(testcircuit.I[0][0], int2seq(1, 8), scope)
+            sim.set_value(testcircuit.CE, 1, scope)
+        else:
+            sim.set_value(testcircuit.I[0][0], int2seq(2, 8), scope)
+            sim.set_value(testcircuit.CE, 0, scope)
+        sim.evaluate()
+        sim.advance_cycle()
+        sim.evaluate()
+        print([seq2int(sim.get_value(testcircuit.O[0][r][c], scope)) for r in range(2) for c in range(2)])
+        print(sim.get_value(testcircuit.valid, scope))
+        # for some reason, lb going to 0 when flickering valid on and off for ce
+        for r in range(2):
+            for c in range(2):
+                assert (sim.get_value(testcircuit.valid, scope) == 0 or seq2int(sim.get_value(testcircuit.O[0][r][c], scope)) != 0)
+
+def test_db_flicker_ce():
+    scope = Scope()
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+
+    testcircuit = DefineDelayedBuffer(cirb, Array(8, Bit), 4, 1, 16, 1)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=c,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    for i in range(100000):
+        if i % 2 == 0 and i > 20:
+            sim.set_value(testcircuit.I[0], int2seq(1, 8), scope)
+            sim.set_value(testcircuit.CE, 1, scope)
+        else:
+            sim.set_value(testcircuit.I[0], int2seq(2, 8), scope)
+            sim.set_value(testcircuit.CE, 0, scope)
+        if i % 2 == 0 and i > 20: #(not i % 16 == 0 or i == 0):
+            sim.set_value(testcircuit.WE, 1, scope)
+        else:
+            sim.set_value(testcircuit.WE, 0, scope)
+        sim.evaluate()
+        print(seq2int(sim.get_value(testcircuit.O[0], scope)))
+        print(sim.get_value(testcircuit.valid, scope))
+
+        # for some reason, lb going to 0 when flickering valid on and off for ce
+        assert (sim.get_value(testcircuit.valid, scope) == 0 or seq2int(sim.get_value(testcircuit.O[0], scope)) != 0)
+        sim.advance_cycle()
+
+def test_ram_flicker_we():
+    scope = Scope()
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+
+    testcircuit = DefineRAM(8, 1)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=c,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    sim.set_value(testcircuit.WADDR[0], 0, scope)
+    sim.set_value(testcircuit.RADDR[0], 0, scope)
+    sim.set_value(testcircuit.WDATA[0], 1, scope)
+    for i in range(20):
+        sim.set_value(testcircuit.WE, 0, scope)
+        sim.evaluate()
+        sim.advance_cycle()
+    sim.set_value(testcircuit.WE, 1, scope)
+    sim.evaluate()
+    sim.advance_cycle()
+    sim.set_value(testcircuit.WE, 0, scope)
+    sim.evaluate()
+    sim.advance_cycle()
+    print(sim.get_value(testcircuit.RDATA, scope))
+
+def test_any_type_ram_flicker_we():
+    scope = Scope()
+    c = coreir.Context()
+    cirb = CoreIRBackend(c)
+
+    testcircuit = DefineRAMAnyType(cirb, Array(4, Array(8, Bit)), 4) #DefineRAM(8, 1)
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK, context=c,
+                          namespaces=["aetherlinglib", "commonlib", "mantle", "coreir", "global"])
+
+    sim.set_value(testcircuit.WADDR[0], 0, scope)
+    sim.set_value(testcircuit.RADDR[0], 0, scope)
+    sim.set_value(testcircuit.WDATA[0], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WDATA[1], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WDATA[2], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WDATA[3], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WE, 0, scope)
+    for i in range(20):
+        sim.evaluate()
+        if i == 0:
+            sim.set_value(testcircuit.WDATA[0], int2seq(1, 8), scope)
+        if i == 2:
+            sim.set_value(testcircuit.WDATA[1], int2seq(1, 8), scope)
+        if i == 16:
+            sim.set_value(testcircuit.WDATA[2], int2seq(1, 8), scope)
+        if i == 18:
+            sim.set_value(testcircuit.WDATA[3], int2seq(1, 8), scope)
+        sim.evaluate()
+        sim.advance_cycle()
+        sim.evaluate()
+        print("WDATA: " + str([seq2int(sim.get_value(testcircuit.WDATA, scope)[r]) for r in range(4)]))
+        print("RDATA: " + str([seq2int(sim.get_value(testcircuit.RDATA, scope)[r]) for r in range(4)]))
+    sim.set_value(testcircuit.WE, 1, scope)
+    sim.evaluate()
+    sim.advance_cycle()
+    sim.evaluate()
+    sim.set_value(testcircuit.WDATA[0], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WDATA[1], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WDATA[2], int2seq(0, 8), scope)
+    sim.set_value(testcircuit.WDATA[3], int2seq(0, 8), scope)
+    print("RDATA: " + str([seq2int(sim.get_value(testcircuit.RDATA, scope)[r]) for r in range(4)]))
+    sim.set_value(testcircuit.WE, 0, scope)
+    sim.evaluate()
+    sim.advance_cycle()
+    sim.evaluate()
+    print("RDATA: " + str([seq2int(sim.get_value(testcircuit.RDATA, scope)[r]) for r in range(4)]))
+
