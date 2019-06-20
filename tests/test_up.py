16893b57e00f282d337a288c5ff9c2347e973117
David Durst <davidbdurst@gmail.com>
2019-06-20 17:19:26 -0400

    Last Sequential Upsample Test Passes
    Sequential Upsample with 2 clocks per element, flickering rv and ce
    passes.

--- tests/test_up.py
+++ tests/test_up.py
@@ -252,10 +252,13 @@ def test_up_sequential_multi_clock_elements_flicker_rv_and_ce():
     num_out = 3
     time_per_element = 2
     num_valid_clocks = num_out * time_per_element
-    num_clocks_per_iteration = num_valid_clocks * 12
+    clks_between_valid_clks = 12
+    num_clocks_per_iteration = num_valid_clocks * clks_between_valid_clks
     num_iterations = 2
     num_total_clocks = num_clocks_per_iteration * num_iterations
-    test_val = 3
+    test_vals = [3,4,5,6]
+    read_test_val_idx = 0
+    write_test_val_idx = 0
     scope = Scope()
     inType = Array[width, In(BitIn)]
     outType = Out(inType)
@@ -277,7 +280,7 @@ def test_up_sequential_multi_clock_elements_flicker_rv_and_ce():
 
     sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
 
-    valid_clks = [i for i in range(num_total_clocks) if i % 12 == 0]
+    valid_clks = [i for i in range(num_total_clocks) if i % clks_between_valid_clks == 0]
 
     # only ready one element per num_out as outputs need to repeat num_out times
     ready_clks = []
@@ -285,20 +288,31 @@ def test_up_sequential_multi_clock_elements_flicker_rv_and_ce():
     for element in range(num_out * num_iterations):
         for i_per_el in range(time_per_element):
             if element % num_out == 0:
-                ready_clks.append(valid_clks[element + i_per_el])
+                ready_clks.append(valid_clks[element * time_per_element + i_per_el])
 
     for clk in range(num_total_clocks):
         sim.set_value(testcircuit.valid_up, clk % 2 == 0, scope)
         sim.set_value(testcircuit.ready_down, clk % 3 == 0, scope)
         sim.set_value(testcircuit.CE, clk % 4 == 0, scope)
-        sim.set_value(testcircuit.I, int2seq(test_val + clk // num_clocks_per_iteration, width), scope)
+        sim.set_value(testcircuit.I, int2seq(test_vals[write_test_val_idx], width), scope)
         sim.evaluate()
         if clk in valid_clks:
-            assert seq2int(sim.get_value(testcircuit.O, scope)) == test_val + clk // num_clocks_per_iteration
+            assert seq2int(sim.get_value(testcircuit.O, scope)) == test_vals[read_test_val_idx]
         assert sim.get_value(testcircuit.valid_down, scope) == (clk in valid_clks)
         assert sim.get_value(testcircuit.ready_up, scope) == (clk in ready_clks)
         sim.advance_cycle()
         sim.evaluate()
+
+        if clk in valid_clks:
+            # only use the inputs for the current input sequence
+            read_test_val_idx += 1
+            read_test_val_idx %= 2
+            read_test_val_idx += 2 * ((clk+clks_between_valid_clks) // num_clocks_per_iteration)
+        if clk in ready_clks:
+            write_test_val_idx += 1
+            # after finishing all the test values, on last ready clock, will need to roll back to start so
+            # don't overflow test_vals array
+            write_test_val_idx %= len(test_vals)
 """
 
 width = 5
