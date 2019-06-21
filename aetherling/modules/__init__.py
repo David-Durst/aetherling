d72b8ac0019f9148a9298a548a5ad389bad7a0d0
David Durst <davidbdurst@gmail.com>
2019-06-21 01:53:29 -0400

    filled out aetherling modules init for suggested imports

---
+++ aetherling/modules/__init__.py
@@ -0,0 +1,9 @@
+from .register_any_type import DefineRegisterAnyType, RegisterAnyType
+from .ram_any_type import DefineRAMAnyType, RAMAnyType
+from .mux_any_type import DefineMuxAnyType, MuxAnyType
+from .term_any_type import DefineTermAnyType, TermAnyType
+from .upsample import DefineUpsampleParallel, UpsampleParallel, DefineUpsampleSequential, UpsampleSequential
+from .downsample import DefineDownsampleParallel, DownsampleParallel, DefineDownsampleSequential, DownsampleSequential
+from .map_fully_parallel_sequential import DefineNativeMapParallel
+from .hydrate import DefineDehydrate, Dehydrate, DefineHydrate, Hydrate
+from .reduce import DefineReduceParallel, ReduceParallel, DefineReduceSequential, ReduceSequential, renameCircuitForReduce
