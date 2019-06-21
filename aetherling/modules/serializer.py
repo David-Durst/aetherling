e27d75f1a77d3ce0ea356aef512f78280afb14a1
David Durst <davidbdurst@gmail.com>
2019-06-21 01:49:32 -0400

    Untested ST Serialize and Deserialize
    Rewrote serialize and deserialize to handle elements that take more than
    one clock. This enables serializing and deserializing nested TSeqs.
    Written in magma rather than as a commonlib wrapper.
    
    New serialize and deserialize are untested.

--- aetherling/modules/serializer.py
+++ aetherling/modules/serializer.py
@@ -6,174 +6,200 @@ from magma.t import Kind
 from mantle.coreir.type_helpers import Term
 from .hydrate import DefineDehydrate, Dehydrate, DefineHydrate, Hydrate
 from .map_fully_parallel_sequential import MapParallel
+from aetherling.helpers.magma_helpers import ready_valid_interface
+from aetherling.modules import DefineRAMAnyType, DefineRegisterAnyType, DefineMuxAnyType, DefineNativeMapParallel
+from mantle.common.countermod import SizedCounterModM
+from mantle.common.decode import Decode
 
 @cache_definition
-def DefineSerializer(T: Kind, N: int, has_count=False, has_ce=False, has_reset=False) -> DefineCircuitKind:
+def DefineSerializer(n:int, time_per_element: int, T: Kind, has_ce=False, has_reset=False) -> DefineCircuitKind:
     """
-    Serializer converts an array to a stream and emit the stream over multiple clocks
-    Aetherling Type: ({1, Array[N, T]} -> {N, T}, N)
-    This returns a circuit definition.
+    Convert sequences in space to sequences in time.
+    TSeq no (vo+no*vi) (SSeq n T') -> TSeq no vo (TSeq n vi T')
 
-    :param cirb: The CoreIR backend currently be used
-    :param T: The type of the elements in the array to serialize
-    :param N: The length of the array to serialize
-    :param has_count: Whether to have a port on serializer of length equal to the
-    width of T in bits that emits the current element being emitted by the serializer
-    :param has_ce: Whether to have a CE port on serializer
-    :param has_reset: Whether to have a reset port on serializer
-    :return: A module with the following ports:
-        I : In(Array[N, T])
-        out : Out(T)
-        ready : Out(Bit)
-        valid : Out(Bit)
-        and others depending on has_ arguments
+    Each T' period is time_per_element clock cycles
+    You can get time_per_element by calling time on a space-time type.
+
+    Note that the T passed to this operator just the Magma type each clock cycle.
+    You can get T by calling magma_repr on a space-time type T'.
+
+    I : In(T)
+    O : Out(T)
+    ready_up : Out(Bit)
+    valid_up : In(Bit)
+    ready_down : In(Bit)
+    valid_down : Out(Bit)
+    if has_ce:
+    CE : In(Bit)
+    if has_reset:
+    RESET : In(Bit)
     """
-    if N <= 0:
-        raise ValueError("Serialize must work on an array of at least 1. N <= 0")
-
     class _Serializer(Circuit):
-        name = "serialize_t{}_n{}".format(cleanName(str(T)), str(N))
+        name = "serialize_t{}_tEl{}_n{}_hasCE{}_hasRESET{}".format(cleanName(str(T)), str(time_per_element), \
+                                                                   str(n), str(has_ce), str(has_reset))
 
-        if has_count:
-            cirType = GetCoreIRBackend().get_type(T)
-            count_interface = ["count", Out(Array[T.size, Bit])]
-        else:
-            count_interface = []
-
-        IO = ["I", In(Array[N, T]), "out", Out(T), "ready", Out(Bit),
-              "valid", Out(Bit)] + \
-             ClockInterface(has_ce, has_reset) + count_interface
+        IO = ["I", In(Array[n, T]), "O", Out(T)] + \
+             ClockInterface(has_ce, has_reset) + ready_valid_interface
 
         @classmethod
         def definition(serializer):
-            # CoreIR serializer works on arrays of arrays of bits, where each T is an array of bits
-            # so need to hydrate and dehydrate
-            dehydrate = MapParallel(N, DefineDehydrate(T))
-            hydrate = Hydrate(T)
-            coreirSerializer = CircuitInstanceFromGeneratorWrapper(GetCoreIRBackend(), "commonlib", "serializer",
-                                                                   "dehydrated_" + serializer.name,
-                                                                   ["mantle", "coreir", "global"],
-                                                                   {"width": hydrate.size, "rate": N})
-
-            wire(serializer.I, dehydrate.I)
-            wire(dehydrate.out, coreirSerializer.I)
-            wire(coreirSerializer.out, hydrate.I)
-            wire(hydrate.out, serializer.out)
-            wire(coreirSerializer.ready, serializer.ready)
-
+            enabled = serializer.ready_down & serializer.valid_up
             if has_ce:
-                wire(serializer.CE, coreirSerializer.en)
-                wire(bit(serializer.CE), serializer.valid)
-            else:
-                wire(VCC, coreirSerializer.en)
-                wire(1, serializer.valid)
+                enabled = enabled & bit(serializer.CE)
+
+            # the counter of the current element of output sequence, when hits 0, load the next input to serialize
+            element_idx_counter = SizedCounterModM(n, has_ce=True, has_reset=has_reset)
+            is_first_element = Decode(0, element_idx_counter.O.N)(element_idx_counter.O)
             if has_reset:
-                wire(serializer.RESET, coreirSerializer.reset)
+                wire(serializer.RESET, element_idx_counter.RESET)
+
+            # if each element takes multiple clocks, need a ram so can write all them and read them over multiple clocks
+            if time_per_element > 1:
+                value_store = DefineNativeMapParallel(n, DefineRAMAnyType(T, time_per_element))()
+                value_store_input = value_store.WDATA
+                value_store_output = value_store.RDATA
+
+                time_per_element_counter = SizedCounterModM(time_per_element,
+                                                            has_ce=True, has_reset=has_reset)
+                go_to_next_element = Decode(time_per_element - 1, time_per_element_counter.O.N)(time_per_element_counter.O)
+
+                wire(time_per_element_counter.CE, enabled)
+                wire(element_idx_counter.CE, enabled & go_to_next_element)
+                for input_idx in range(n):
+                    wire(value_store[input_idx].WE, is_first_element & enabled)
+                    # location in current element is where to read and write.
+                    # will write on first iteration through element, read on later iterations
+                    wire(time_per_element_counter.O, value_store[input_idx].WADDR)
+                    wire(time_per_element_counter.O, value_store[input_idx].RADDR)
+
+                if has_reset:
+                    wire(time_per_element_counter.RESET, serializer.RESET)
+
             else:
-                wire(GND, coreirSerializer.reset)
-            if has_count:
-                wire(coreirSerializer.count, serializer.count)
-            else:
-                countTerm = Term(hydrate.size)
-                wire(coreirSerializer.count, countTerm.I)
+                value_store = DefineNativeMapParallel(n, DefineRegisterAnyType(T, has_ce=True))()
+                value_store_input = value_store.I
+                value_store_output = value_store.O
+
+                wire(element_idx_counter.CE, enabled)
+                for input_idx in range(n):
+                    wire(value_store.CE, is_first_element & enabled)
+
+
+            for input_idx in range(n):
+                wire(serializer.I, value_store_input[input_idx])
+
+            # to serialize, go through all different rams/registers in value store
+            # and select the output from the ith one, where i is current output element
+            value_store_output_selector = DefineMuxAnyType(T, n)()
+            wire(value_store_output, value_store_output_selector.data)
+            wire(element_idx_counter.O, value_store_output_selector.sel)
+
+            # on first element, send the input directly out. otherwise, use the register
+            first_element_output_selector = DefineMuxAnyType(T, 2)()
+            wire(is_first_element, first_element_output_selector.sel[0])
+            wire(value_store_output_selector, first_element_output_selector.data[0])
+            wire(serializer.I, first_element_output_selector.data[1])
+            wire(first_element_output_selector.out, serializer.O)
+
+            wire(enabled, serializer.valid_down)
+            wire(enabled & is_first_element, serializer.ready_up)
 
     return _Serializer
 
-def Serializer(T: Kind, N: int, has_count=False, has_ce=False, has_reset=False) -> Circuit:
-    """
-    Serializer converts an array to a stream and emit the stream over multiple clocks
-    Aetherling Type: ({1, Array[N, T]} -> {N, T}, N)
-    This returns a circuit instance, an instance of a circuit definition
-
-    :param cirb: The CoreIR backend currently be used
-    :param T: The type of the elements in the array to serialize
-    :param N: The length of the array to serialize
-    :param has_count: Whether to have a port on serializer of length equal to the
-    width of T in bits that emits the current element being emitted by the serializer
-    :param has_ce: Whether to have a CE port on serializer
-    :param has_reset: Whether to have a reset port on serializer
-    :return: A module with the following ports:
-        I : In(Array[N, T])
-        out : Out(T)
-        ready : Out(Bit)
-        valid : Out(Bit)
-        and others depending on has_ arguments
-    """
-    return DefineSerializer(T, N, has_count, has_ce, has_reset)()
+def Serializer(n:int, time_per_element: int, T: Kind, has_ce=False, has_reset=False) -> Circuit:
+    return DefineSerializer(n, time_per_element, T, has_ce, has_reset)()
 
 @cache_definition
-def DefineDeserializer(T: Kind, N: int, has_ce=False, has_reset=False) -> DefineCircuitKind:
+def DefineDeserializer(n: int, time_per_element: int, T: Kind, has_ce=False, has_reset=False) -> DefineCircuitKind:
     """
-    Deserializer converts a stream to an array over multiple clocks
-    Aetherling Type: ({N, T} -> {1, Array[N, T]}, N)
-    This returns a circuit definition.
+    Convert sequences in time to sequences in space.
+    TSeq no vo (TSeq n vi T') -> TSeq no (vo+no*vi) (SSeq n T')
 
-    :param cirb: The CoreIR backend currently be used
-    :param T: The type of the elements in the array to deserialize
-    :param N: The length of the array to deserializer
-    :param has_ce: Whether to have a CE port on deserializer
-    :param has_reset: Whether to have a reset port on deserializer
-    :return: A module with the following ports:
-        I : In(T)
-        out : Out(Array[N, T])
-        ready : Out(Bit)
-        valid : Out(Bit)
-        and others depending on has_ arguments
+    Each T' period is time_per_element clock cycles
+    You can get time_per_element by calling time on a space-time type.
+
+    Note that the T passed to this operator just the Magma type each clock cycle.
+    You can get T by calling magma_repr on a space-time type T'.
+
+    I : In(T)
+    O : Out(T)
+    ready_up : Out(Bit)
+    valid_up : In(Bit)
+    ready_down : In(Bit)
+    valid_down : Out(Bit)
+    if has_ce:
+    CE : In(Bit)
+    if has_reset:
+    RESET : In(Bit)
     """
-    if N <= 0:
-        raise ValueError("Serialize must work on an array of at least 1. N <= 0")
 
     class _Deserializer(Circuit):
-        name = "serialize_t{}_n{}".format(cleanName(str(T)), str(N))
+        name = "deserialize_t{}_tEl{}_n{}_hasCE{}_hasRESET{}".format(cleanName(str(T)), str(time_per_element), \
+                                                                     str(n), str(has_ce), str(has_reset))
 
-        IO = ["I", In(T), "out", Out(Array[N, T]), "valid", Out(Bit),
-              "ready", Out(Bit)] + ClockInterface(has_ce, has_reset)
+        IO = ["I", In(Array[n, T]), "out", Out(T)] + \
+             ClockInterface(has_ce, has_reset) + ready_valid_interface
 
         @classmethod
-        def definition(serializer):
-            # CoreIR serializer works on arrays of arrays of bits, where each T is an array of bits
-            # so need to hydrate and dehydrate
-            dehydrate = Dehydrate(T)
-            hydrate = MapParallel(N, DefineHydrate(T))
-            coreirDeserializer = CircuitInstanceFromGeneratorWrapper(GetCoreIRBackend(), "commonlib", "deserializer",
-                                                                     "dehydrated_" + serializer.name,
-                                                                     ["mantle", "coreir", "global"],
-                                                                     {"width": dehydrate.size, "rate": N})
-
-            wire(serializer.I, dehydrate.I)
-            wire(dehydrate.out, coreirDeserializer.I)
-            wire(coreirDeserializer.out, hydrate.I)
-            wire(hydrate.out, serializer.out)
-            wire(coreirDeserializer.valid, serializer.valid)
-            wire(serializer.ready, 1)
-
+        def definition(deserializer):
+            enabled = deserializer.ready_down & deserializer.valid_up
             if has_ce:
-                wire(serializer.CE, coreirDeserializer.en)
-            else:
-                wire(VCC, coreirDeserializer.en)
+                enabled = enabled & bit(deserializer.CE)
+
+            # the counter of the current element of output sequence, when hits 0, load the next input to serialize
+            element_idx_counter = SizedCounterModM(n, has_ce=True, has_reset=has_reset)
+            is_last_element = Decode(n-1, element_idx_counter.O.N)(element_idx_counter.O)
             if has_reset:
-                wire(serializer.RESET, coreirDeserializer.reset)
+                wire(deserializer.RESET, element_idx_counter.RESET)
+
+            # if each element takes multiple clocks, need a ram so can write all them and read them over multiple clocks
+            if time_per_element > 1:
+                # only use n-1 value store, just wire nth input directly to output since outputting whole
+                # deserialized sequence on period receiving nth input
+                value_store = DefineNativeMapParallel(n-1, DefineRAMAnyType(T, time_per_element))()
+                value_store_input = value_store.WDATA
+                value_store_output = value_store.RDATA
+                value_store_enables = value_store.CE
+
+                time_per_element_counter = SizedCounterModM(time_per_element,
+                                                            has_ce=True, has_reset=has_reset)
+                go_to_next_element = Decode(time_per_element - 1, time_per_element_counter.O.N)(time_per_element_counter.O)
+
+                wire(time_per_element_counter.CE, enabled)
+                wire(element_idx_counter.CE, enabled & go_to_next_element)
+                for input_idx in range(n-1):
+                    # location in current element is where to read and write.
+                    # will write on first iteration through each element, read on last iteration from all elements
+                    wire(time_per_element_counter.O, value_store[input_idx].WADDR)
+                    wire(time_per_element_counter.O, value_store[input_idx].RADDR)
+
+                if has_reset:
+                    wire(time_per_element_counter.RESET, deserializer.RESET)
+
             else:
-                wire(GND, coreirDeserializer.reset)
+                value_store = DefineNativeMapParallel(n-1, DefineRegisterAnyType(T, has_ce=True))()
+                value_store_input = value_store.I
+                value_store_output = value_store.O
+                value_store_enables = value_store.CE
+                wire(element_idx_counter.CE, enabled)
+
+            for element_idx in range(n-1):
+                # send input to all value stores
+                # the enables will ensure only the right store each period reads in the value
+                wire(deserializer.I, value_store_input[element_idx])
+                # to deserialize, enable the ith rams/registers in value store
+                # for ith element input
+                idx_match_cur_element = Decode(element_idx, element_idx_counter.O.N)(element_idx_counter.O)
+                wire(enabled & idx_match_cur_element, value_store_enables[element_idx])
+                wire(value_store_output[element_idx], deserializer.O[element_idx])
+
+            # send the last input directly out
+            wire(deserializer.I[n-1], deserializer.O[n-1])
+
+            wire(enabled & is_last_element, deserializer.valid_down)
+            wire(enabled, deserializer.ready_up)
 
     return _Deserializer
 
-def Deserializer(T: Kind, N: int, has_ce=False, has_reset=False) -> Circuit:
-    """
-    Deserializer converts a stream to an array over multiple clocks
-    Aetherling Type: ({N, T} -> {1, Array[N, T]}, N)
-    This returns a circuit instance, an instance of a circuit definition
-
-    :param cirb: The CoreIR backend currently be used
-    :param T: The type of the elements in the array to deserialize
-    :param N: The length of the array to deserializer
-    :param has_ce: Whether to have a CE port on deserializer
-    :param has_reset: Whether to have a reset port on deserializer
-    :return: A module with the following ports:
-        I : In(T)
-        out : Out(Array[N, T])
-        ready : Out(Bit)
-        valid : Out(Bit)
-        and others depending on has_ arguments
-    """
-    return DefineDeserializer(T, N, has_ce, has_reset)()
+def Deserializer(n: int, time_per_element: int, T: Kind, has_ce=False, has_reset=False) -> Circuit:
+    return DefineDeserializer(n, time_per_element, T, has_ce, has_reset)()
