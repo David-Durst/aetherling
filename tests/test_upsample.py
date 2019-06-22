ff49fe0c747c994eee7077adcd9f5e21dcc19c14
David Durst <davidbdurst@gmail.com>
2019-06-22 17:50:54 -0400

    trying to fix upsample tests for more complex rv pattern

--- tests/test_upsample.py
+++ tests/test_upsample.py
@@ -127,13 +127,47 @@ def test_up_sequential_rv_always_true_no_ce():
         sim.advance_cycle()
         sim.evaluate()
 
+def get_clock_pattern(total_clocks, ce_period, rv_period):
+    """
+    Get the cycles for when to check ready or valid
+    :param total_clocks: total number of clock cycles to return the pattern for
+    :param ce_period: period of every nth clock cycle that CE is set
+    :param rv_period: period of every rv_period clock cycle that either ready, valid, or ready and valid is set
+    :return:
+    """
+    clock_pattern = []
+    for i in range(total_clocks):
+        if i % ce_period == 0 and i % rv_period == 0:
+            clock_pattern.append(i)
+    return clock_pattern
+
+def filter_clock_pattern(input_clock_pattern, time_per_element, included_elements):
+    """
+    Remove cycles from a pattern if only some are going to be used.
+    This is used for upsample sequential's ready_in as it is only high for the first element.
+    Each element may take multiple clocks.
+    :param input_clock_pattern: array of elements to use
+    :param time_per_element: clock cycles per element
+    :param included_elements: elements whose clocks should be included in result
+    :return:
+    """
+    filtered_clock_pattern = []
+    num_elements = len(input_clock_pattern) // time_per_element
+    for el_idx in range(num_elements):
+        if el_idx in included_elements:
+            filtered_clock_pattern += input_clock_pattern[el_idx * time_per_element:(el_idx + 1) * time_per_element]
+    return filtered_clock_pattern
 
 def test_up_sequential_rv_and_ce_flicker():
     width = 5
     num_out = 3
     time_per_element = 1
     num_valid_clocks = num_out * time_per_element
-    num_clocks_per_iteration = num_valid_clocks * 12
+    ce_period = 2
+    ready_down_period = 2
+    valid_up_period = 6
+    # 6 is least common multiple of all periods
+    num_clocks_per_iteration = num_valid_clocks * 6
     num_iterations = 2
     num_total_clocks = num_clocks_per_iteration * num_iterations
     test_val = 3
@@ -158,28 +192,28 @@ def test_up_sequential_rv_and_ce_flicker():
 
     sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
 
-    valid_clks = [i for i in range(num_total_clocks) if i % 12 == 0]
+    valid_clks = get_clock_pattern(num_total_clocks, ce_period, valid_up_period)
 
     # only ready one element per num_out as outputs need to repeat num_out times
-    ready_clks = []
-    # multiply
-    for element in range(num_out * num_iterations):
-        for i_per_el in range(time_per_element):
-            if element % num_out == 0:
-                ready_clks.append(valid_clks[element + i_per_el])
+    ready_clks = filter_clock_pattern(get_clock_pattern(num_total_clocks, ce_period, ready_down_period), time_per_element, [0])
 
-    for clk in range(num_total_clocks):
-        sim.set_value(testcircuit.valid_up, clk % 2 == 0, scope)
-        sim.set_value(testcircuit.ready_down, clk % 3 == 0, scope)
-        sim.set_value(testcircuit.CE, clk % 4 == 0, scope)
-        sim.set_value(testcircuit.I, int2seq(test_val + clk // num_clocks_per_iteration, width), scope)
-        sim.evaluate()
-        if clk in valid_clks:
-            assert seq2int(sim.get_value(testcircuit.O, scope)) == test_val + clk // num_clocks_per_iteration
-        assert sim.get_value(testcircuit.valid_down, scope) == (clk in valid_clks)
-        assert sim.get_value(testcircuit.ready_up, scope) == (clk in ready_clks)
-        sim.advance_cycle()
-        sim.evaluate()
+    for i in range(num_iterations):
+        for clk in range(num_clocks_per_iteration):
+            sim.set_value(testcircuit.valid_up, clk % valid_up_period == 0, scope)
+            sim.set_value(testcircuit.ready_down, clk % ready_down_period == 0, scope)
+            sim.set_value(testcircuit.CE, clk % ce_period == 0, scope)
+            sim.set_value(testcircuit.I, int2seq(test_val + i, width), scope)
+            sim.evaluate()
+            if clk in ready_clks:
+                assert sim.get_value(testcircuit.ready_up, scope) == True
+            # ensure that can change input on not ready clock and not affect anything
+            else:
+                sim.set_value(testcircuit.I, int2seq(0, width), scope)
+            if clk in valid_clks:
+                assert seq2int(sim.get_value(testcircuit.O, scope)) == test_val + i
+                assert sim.get_value(testcircuit.valid_down, scope) == True
+            sim.advance_cycle()
+            sim.evaluate()
 
 def test_up_sequential_multi_clock_elements():
     width = 5
@@ -213,15 +247,10 @@ def test_up_sequential_multi_clock_elements():
 
     sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
 
-    valid_clks = [i for i in range(num_total_clocks)]
+    valid_clks = get_clock_pattern(num_total_clocks, 1, 1)
 
     # only ready one element per num_out as outputs need to repeat num_out times
-    ready_clks = []
-    # multiply
-    for element in range(num_out * num_iterations):
-        for i_per_el in range(time_per_element):
-            if element % num_out == 0:
-                ready_clks.append(valid_clks[element * time_per_element + i_per_el])
+    ready_clks = filter_clock_pattern(get_clock_pattern(num_total_clocks, 1, 1), time_per_element, [0, 3])
 
     sim.set_value(testcircuit.valid_up, True, scope)
     sim.set_value(testcircuit.ready_down, True, scope)
@@ -252,6 +281,9 @@ def test_up_sequential_multi_clock_elements_flicker_rv_and_ce():
     num_out = 3
     time_per_element = 2
     num_valid_clocks = num_out * time_per_element
+    valid_period = 2
+    ready_period = 3
+    ce_period = 4
     clks_between_valid_clks = 12
     num_clocks_per_iteration = num_valid_clocks * clks_between_valid_clks
     num_iterations = 2
@@ -280,15 +312,10 @@ def test_up_sequential_multi_clock_elements_flicker_rv_and_ce():
 
     sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
 
-    valid_clks = [i for i in range(num_total_clocks) if i % clks_between_valid_clks == 0]
+    valid_clks = get_clock_pattern(num_total_clocks, ce_period, valid_period)
 
     # only ready one element per num_out as outputs need to repeat num_out times
-    ready_clks = []
-    # multiply
-    for element in range(num_out * num_iterations):
-        for i_per_el in range(time_per_element):
-            if element % num_out == 0:
-                ready_clks.append(valid_clks[element * time_per_element + i_per_el])
+    ready_clks = filter_clock_pattern(get_clock_pattern(num_total_clocks, ce_period, ready_period), time_per_element, [0, 3])
 
     for clk in range(num_total_clocks):
         sim.set_value(testcircuit.valid_up, clk % 2 == 0, scope)
