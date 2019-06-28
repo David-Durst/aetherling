f9cae8efe4a4b412da0b22026b5e3b5ef8d56a90
David Durst <davidbdurst@gmail.com>
2019-06-27 21:55:34 -0700

    added unnecessary rshift complexity, don't need to handle multiple elements

--- aetherling/modules/rshift.py
+++ aetherling/modules/rshift.py
@@ -2,6 +2,7 @@ from mantle import Register, Decode
 from mantle.common.countermod import SizedCounterModM
 from mantle.common.operator import *
 from aetherling.modules.term_any_type import TermAnyType
+from aetherling.modules.fifo import FIFO
 from magma import *
 from magma.circuit import DefineCircuitKind
 from .hydrate import Dehydrate, Hydrate
@@ -15,7 +16,7 @@ __all__ = ['DefineRShiftParallel', 'RShiftParallel',
 @cache_definition
 def DefineRShiftParallel(n: int, init: DefineCircuitKind, shift_amount: int, T: Kind, has_ready_valid=False):
     """
-    Shifts the elements in SSeq n T' by shift_amount to the right combinationally.
+    Shifts the elements in SSeq n T' by shift_amount to the right.
     init is a Magma circuit that specifies the outputs used for the shift_amount elements to the right
 
     The time_per_element clock cycles in a period is not relevant for this operator as it is combinational.
@@ -58,73 +59,91 @@ def RShiftParallel(n: int, init: DefineCircuitKind, shift_amount: int, T: Kind, 
 @cache_definition
 def DefineRShiftSequential(n, time_per_element, idx, T, has_ce=False, has_reset=False):
     """
-    Downsample a TSeq n T' to a TSeq 1 T' over n period.
+    Shifts the elements in TSeq n T' by shift_amount to the right.
+    init is a Magma circuit that specifies the outputs used for the shift_amount elements to the right
 
-    Each T' period is time_per_element clock cycles
-    You can get time_per_element by calling time on a space-time type.
+    The time_per_element clock cycles in a period is not relevant for this operator as it is combinational.
 
     Note that the T passed to this operator just the Magma type each clock cycle.
     You can get T by calling magma_repr on a space-time type T'.
 
-    I : In(T)
-    O : Out(T)
+    I : In(Array[n, T])
+    O : Out(Array[n, T])
+    if has_ready_valid:
     ready_up : Out(Bit)
     valid_up : In(Bit)
     ready_down : In(Bit)
     valid_down : Out(Bit)
-    if has_ce:
-    CE : In(Bit)
-    if has_reset:
-    RESET : In(Bit)
     """
     class DownsampleSequential(Circuit):
         name = "DownsampleSequential_n{}_tEl{}_idx{}_T{}_hasCE{}_hasReset{}".format(str(n), str(time_per_element), \
                                                                                     str(idx), cleanName(str(T)), str(has_ce), str(has_reset))
         IO = ['I', In(T), 'O', Out(T)] + ClockInterface(has_ce, has_reset) + ready_valid_interface
         @classmethod
-        def definition(downsampleSequential):
+        def definition(rshiftSequential):
+            # the counter of the current element of output sequence, when hits 0, load the next input to upsample
             element_idx_counter = SizedCounterModM(n, has_ce=True, has_reset=has_reset)
-            emit_cur_element = Decode(idx, element_idx_counter.O.N)(element_idx_counter.O)
+            is_first_element = Decode(0, element_idx_counter.O.N)(element_idx_counter.O)
+            if has_reset:
+                wire(rshiftSequential.RESET, element_idx_counter.RESET)
 
-            # enabled means run the circuit
-            # do this when upstream is ready, so have something to downsample,
-            # and when have to emit current element and downstream is ready or don't have to emit current element
-            enabled = downsampleSequential.valid_up & \
-                      ((emit_cur_element & downsampleSequential.ready_down) | (~emit_cur_element))
             # ready means can accept input when get valid from upstream
-            # ready when emit current element and downstream is ready or don't have to emit current element
-            ready = (emit_cur_element & downsampleSequential.ready_down) | (~emit_cur_element)
+            # do this when in first element or downstream ready to accept
+            ready = is_first_element & rshiftSequential.ready_down
             # valid means can emit downstream
-            # valid when emitting current element and upstream is providing valid input for element
-            valid = emit_cur_element & downsampleSequential.valid_up
+            # valid when in first element and upstream valid or repeating old data
+            valid = (is_first_element & rshiftSequential.valid_up) | (~is_first_element)
 
+            # only assert these signals when CE is high or no CE
             if has_ce:
-                enabled = enabled & bit(downsampleSequential.CE)
-                ready = ready & bit(downsampleSequential.CE)
-                valid = valid & bit(downsampleSequential.CE)
+                enabled = enabled & bit(rshiftSequential.CE)
+                ready = ready & bit(rshiftSequential.CE)
+                valid = valid & bit(rshiftSequential.CE)
 
-            if time_per_element > 1:
+            if n * time_per_element > 1:
+                value_store = FIFO(n, time_per_element, T, True, has_ce, has_reset)()
+                value_store_input = value_store.WDATA
+                value_store_output = value_store.RDATA
+
                 time_per_element_counter = SizedCounterModM(time_per_element,
-                                                            has_ce=True,
-                                                            has_reset=has_reset)
+                                                            has_ce=True, has_reset=has_reset)
                 go_to_next_element = Decode(time_per_element - 1, time_per_element_counter.O.N)(time_per_element_counter.O)
 
                 wire(time_per_element_counter.CE, enabled)
                 wire(element_idx_counter.CE, enabled & go_to_next_element)
+                wire(value_store.WE, is_first_element & enabled)
+                # location in current element is where to read and write.
+                # will write on first iteration through element, read on later iterations
+                wire(time_per_element_counter.O, value_store.WADDR)
+                wire(time_per_element_counter.O, value_store.RADDR)
+
+                if has_ce:
+                    wire(value_store.ce, rshiftSequential.CE)
 
                 if has_reset:
-                    wire(time_per_element_counter.RESET, downsampleSequential.RESET)
-                    wire(element_idx_counter.RESET, downsampleSequential.RESET)
+                    wire(time_per_element_counter.RESET, rshiftSequential.RESET)
+
             else:
+                value_store = DefineRegisterAnyType(T, has_ce=True)()
+                value_store_input = value_store.I
+                value_store_output = value_store.O
+
                 wire(element_idx_counter.CE, enabled)
-                if has_reset:
-                    wire(element_idx_counter.RESET, downsampleSequential.RESET)
+                wire(value_store.CE, is_first_element & enabled)
 
-            wire(downsampleSequential.I, downsampleSequential.O)
-            wire(valid, downsampleSequential.valid_down)
-            wire(ready, downsampleSequential.ready_up)
+            output_selector = DefineMuxAnyType(T, 2)()
 
+            wire(rshiftSequential.I, value_store_input)
+
+            # on first element, send the input directly out. otherwise, use the register
+            wire(is_first_element, output_selector.sel[0])
+            wire(value_store_output, output_selector.data[0])
+            wire(rshiftSequential.I, output_selector.data[1])
+            wire(output_selector.out, rshiftSequential.O)
+
+            wire(valid, rshiftSequential.valid_down)
+            wire(ready, rshiftSequential.ready_up)
     return DownsampleSequential
 
-def DownsampleSequential(n, time_per_element, idx, T, has_ce=False, has_reset=False):
-    return DefineDownsampleSequential(n, time_per_element, idx, T, has_ce, has_reset)()
+def RShiftSequential(n, time_per_element, idx, T, has_ce=False, has_reset=False):
+    return DefineRShiftSequential(n, time_per_element, idx, T, has_ce, has_reset)()
