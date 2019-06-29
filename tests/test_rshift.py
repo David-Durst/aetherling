b88c76f1750fbefa7ca0f4a656fcce4f1b9793d3
David Durst <davidbdurst@gmail.com>
2019-06-28 23:02:19 -0700

    rshift parallel tests using coreir simulator pass

--- tests/test_rshift.py
+++ tests/test_rshift.py
@@ -3,9 +3,35 @@ from magma import *
 from magma.clock import *
 from magma.bitutils import *
 from magma.simulator.coreir_simulator import CoreIRSimulator
+from magma.scope import Scope
 import fault
 
 def test_rshift_parallel():
+    width = 5
+    num_in = 4
+    test_vals = [2,5,3,8]
+    shift_amount = 2
+    in_type = Array[num_in, Array[width, In(BitIn)]]
+    scope = Scope()
+    args = ['I', in_type, 'O', Out(in_type)] + ClockInterface(False, False)
+
+    testcircuit = DefineCircuit('Test', *args)
+
+    rshift = RShiftParallel(num_in, shift_amount, in_type.T)
+    wire(rshift.I, testcircuit.I)
+    wire(testcircuit.O, rshift.O)
+
+    EndCircuit()
+
+    sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
+
+    for i, val in enumerate(test_vals):
+        sim.set_value(testcircuit.I[i], int2seq(val, width), scope)
+    sim.evaluate()
+    for i, val in enumerate(test_vals[shift_amount:]):
+        assert seq2int(sim.get_value(testcircuit.O[i + shift_amount])) == test_vals[i]
+
+def fault_test_rshift_parallel():
     width = 5
     num_in = 4
     test_vals = [2,5,3,8]
@@ -34,41 +60,41 @@ def test_rshift_parallel():
         tester.circuit.O[i + shift_amount].expect(test_vals[i])
     tester.compile_and_run(target="verilator", skip_compile=True, directory="vBuild/")
 
-"""
-def test_up_parallel_alternate_rv():
+def test_rshift_parallel_alternate_rv():
     width = 5
-    numOut = 2
-    testVal = 3
-    num_clocks = 12
+    num_in = 4
+    test_vals = [2,5,3,8]
+    shift_amount = 2
+    num_clocks = 6
     scope = Scope()
-    inType = Array[width, In(BitIn)]
-    outType = Array[numOut, Out(inType)]
-    args = ['I', inType, 'O', outType, 'ready_up', Out(Bit), 'valid_up', In(Bit), 'ready_down', In(Bit),
+    in_type = Array[num_in, Array[width, In(BitIn)]]
+    args = ['I', in_type, 'O', Out(in_type), 'ready_up', Out(Bit), 'valid_up', In(Bit), 'ready_down', In(Bit),
             'valid_down', Out(Bit)] + ClockInterface(False, False)
 
     testcircuit = DefineCircuit('Test', *args)
 
-    upParallel = UpsampleParallel(numOut, inType, has_ready_valid=True)
-    wire(upParallel.I, testcircuit.I)
-    wire(testcircuit.O, upParallel.O)
-    wire(testcircuit.ready_up, upParallel.ready_up)
-    wire(testcircuit.valid_up, upParallel.valid_up)
-    wire(testcircuit.ready_down, upParallel.ready_down)
-    wire(testcircuit.valid_down, upParallel.valid_down)
+    rshift = RShiftParallel(num_in, shift_amount, in_type.T, True)
+    wire(rshift.I, testcircuit.I)
+    wire(testcircuit.O, rshift.O)
+    wire(testcircuit.ready_up, rshift.ready_up)
+    wire(testcircuit.valid_up, rshift.valid_up)
+    wire(testcircuit.ready_down, rshift.ready_down)
+    wire(testcircuit.valid_down, rshift.valid_down)
 
     EndCircuit()
 
     sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
 
-    sim.set_value(testcircuit.I, int2seq(testVal, width), scope)
+    sim.evaluate()
     for clk in range(num_clocks):
+        for i, val in enumerate(test_vals):
+            sim.set_value(testcircuit.I[i], int2seq(val + clk * 3, width), scope)
         sim.set_value(testcircuit.valid_up, clk % 3 == 0, scope)
         sim.set_value(testcircuit.ready_down, clk % 2 == 0, scope)
         sim.evaluate()
-        for i in range(numOut):
-            assert seq2int(sim.get_value(testcircuit.O[i], scope)) == testVal
+        for i, val in enumerate(test_vals[shift_amount:]):
+            assert seq2int(sim.get_value(testcircuit.O[i + shift_amount])) == test_vals[i] + clk * 3
         assert sim.get_value(testcircuit.valid_down, scope) == (clk % 3 == 0)
         assert sim.get_value(testcircuit.ready_up, scope) == (clk % 2 == 0)
         sim.advance_cycle()
         sim.evaluate()
-"""
