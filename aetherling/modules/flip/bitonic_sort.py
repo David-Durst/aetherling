861ccd0d67b86113178ee2ed0ca2ca7316a19405
David Durst <davidbdurst@gmail.com>
2019-08-28 16:03:38 -0700

    added bitoinc merge and bitonic sort

--- aetherling/modules/flip/bitonic_sort.py
+++ aetherling/modules/flip/bitonic_sort.py
@@ -16,6 +16,92 @@ from mantle.common.countermod import SizedCounterModM, DefineCounterModM
 from mantle.coreir.compare import DefineCoreirUlt
 from mantle import Decode
 from typing import Callable
+from math import log2
+
+@cache_definition
+def DefineBitonicSortPow2(T: Kind, n: int, sort2LtDef: DefineCircuitKind):
+    """
+
+    :param T: The type of each element in the sort
+    :param n: The number of elements to sort
+    :param sort2LtDef: A circuit definition that accepts two inputs of type T and emits the lesser on out O0
+    and greater on out O1.
+    :return: A circuit with ports
+    I : Array[n, T]
+    O : Array[n, T]
+
+    """
+    class _BitonicSortPow2(Circuit):
+        name = "BitonicMergePow2_t{}_n{}_sort2LtDef{}".format(cleanName(str(T)), str(n), cleanName((str(sort2LtDef))))
+        IO = ['I', In(Array[n, T]), 'O', Out(Array[n, T])]
+        @classmethod
+        def definition(BitonicSortPow2):
+            # input ports and all other intermediate merge ports
+            # starting with just input ports
+            ports = [[BitonicSortPow2.I[i] for i in range(n)]]
+            # Sort ranges starting with 2^1 until 2^m == n
+            for i in range(1,int(log2(n))):
+                elements_per_merge = pow(2, i)
+                cur_prior_stage_port = 0
+                cur_stage_ports = []
+                for j in range(0,i):
+                    merger = DefineBitonicMergePow2(T, elements_per_merge, sort2LtDef, j)
+                    for k in range(0,elements_per_merge):
+                        wire(ports[i][cur_prior_stage_port], merger.I[k])
+                        cur_prior_stage_port += 1
+                        cur_stage_ports += [merger.O[k]]
+            last_ports = ports[-1]
+            for i in range(len(last_ports)):
+                wire(last_ports[i], BitonicSortPow2.O[i])
+    return _BitonicSortPow2
+
+@cache_definition
+def DefineBitonicMergePow2(T: Kind, n: int, sort2LtDef: DefineCircuitKind, ith_merge: int = 0):
+    """
+
+    :param T: The type of each element in the sort
+    :param n: The number of elements to sort
+    :param sort2LtDef: A circuit definition that accepts two inputs of type T and emits the lesser on out O0
+    and greater on out O1.
+    :param ith_merge: The index of this merge. If multiple merge's aligned vertically, alternate directions
+    :return: A circuit with ports
+    I : Array[n, T]
+    O : Array[n, T]
+
+    """
+    class _BitonicMergePow2(Circuit):
+        name = "BitonicMergePow2_t{}_n{}_sort2LtDef{}_ithMerge".format(cleanName(str(T)), str(n),
+                                                                       cleanName((str(sort2LtDef))), str(ith_merge))
+        IO = ['I', In(Array[n, T]), 'O', Out(Array[n, T])]
+        @classmethod
+        def definition(BitonicMergePow2):
+            # first sort the inputs once
+            first_sorts = []
+            for i in range(n // 2):
+                pair_sort = sort2LtDef()
+                if ith_merge % 2 == 0:
+                    wire(BitonicMergePow2.I[i*2], pair_sort.I0)
+                    wire(BitonicMergePow2.I[i*2+1], pair_sort.I1)
+                else:
+                    wire(BitonicMergePow2.I[i*2], pair_sort.I1)
+                    wire(BitonicMergePow2.I[i*2+1], pair_sort.I0)
+                first_sorts += [pair_sort]
+
+            # next merge each of the halfs
+            mergers = [DefineBitonicMergePow2(T, n // 2, sort2LtDef, ith_merge)(),
+                       DefineBitonicMergePow2(T, n // 2, sort2LtDef, ith_merge)()]
+            for i in range(n // 4):
+               wire(pair_sort[i*2].O0, mergers[0].I[i*2])
+               wire(pair_sort[i*2].O1, mergers[0].I[i*2+1])
+               wire(mergers[0].O[i*2], BitonicMergePow2.O[i*2])
+               wire(mergers[0].O[i*2+1], BitonicMergePow2.O[i*2+1])
+
+               wire(pair_sort[(n // 2) + i*2].O0, mergers[1].I[i*2])
+               wire(pair_sort[(n // 2) + i*2].O1, mergers[1].I[i*2+1])
+               wire(mergers[1].O[i*2], BitonicMergePow2.O[(n // 2) + i*2])
+               wire(mergers[1].O[i*2+1], BitonicMergePow2.O[(n // 2) + i*2+1])
+
+    return _BitonicMergePow2
 
 
 @cache_definition
