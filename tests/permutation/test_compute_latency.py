d3527d4e3a5ae82f0b562b1c612132cc72650a1f
David Durst <davidbdurst@gmail.com>
2019-09-05 00:42:27 -0700

    added test that latency compute handles invalids correctly

--- tests/permutation/test_compute_latency.py
+++ tests/permutation/test_compute_latency.py
@@ -8,6 +8,13 @@ def test_flip_latency():
     output_latencies = get_output_latencies(graph)
     assert output_latencies == [2,3,4]
 
+def test_nested_vs_unnested_invalids_latency():
+    input_type = ST_TSeq(2, 0, ST_TSeq(1, 1,  ST_Tombstone()))
+    output_type = ST_TSeq(2, 2, ST_Tombstone())
+    graph = build_input_output_graph(input_type, output_type)
+    output_latencies = get_output_latencies(graph)
+    assert output_latencies == [2,3,4,5]
+
 def test_T4_S2_T4_banks():
     t_len_1 = 4
     t_len_2 = 4
