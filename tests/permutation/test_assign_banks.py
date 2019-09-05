8a119ec2aadb6f5a431acdf23b7d6df9b44ae046
David Durst <davidbdurst@gmail.com>
2019-09-04 17:38:49 -0700

    refactored check_banks function, added case that is failing on address assignment

--- tests/permutation/test_assign_banks.py
+++ tests/permutation/test_assign_banks.py
@@ -9,14 +9,8 @@ def test_2_3_flip_banks():
     output_type = ST_TSeq(t_len, 0, ST_SSeq(s_len, ST_Tombstone()))
     graph = build_input_output_graph(input_type, output_type)
     fixed_graph = assign_banks(graph)
-    for t in range(t_len):
-        # assert that all banks are used and are unique so no conflicts on input or output
-        assert sorted(fixed_graph.input_nodes[t].edge_banks) == list(range(s_len))
-        assert sorted(fixed_graph.output_nodes[t].edge_banks) == list(range(s_len))
-        for s in range(s_len):
-            output_addr = get_output_address_at_input(t, s, fixed_graph.input_type, fixed_graph.output_type)
-            assert fixed_graph.input_nodes[t].edge_banks[s] == \
-                   fixed_graph.output_nodes[output_addr.t].edge_banks[output_addr.s]
+    check_banks(fixed_graph, s_len, t_len)
+
 
 def test_4_6_flip_banks():
     t_len = 4
@@ -25,14 +19,17 @@ def test_4_6_flip_banks():
     output_type = ST_TSeq(t_len, 0, ST_SSeq(s_len, ST_Tombstone()))
     graph = build_input_output_graph(input_type, output_type)
     fixed_graph = assign_banks(graph)
-    for t in range(t_len):
-        # assert that all banks are used and are unique so no conflicts on input or output
-        assert sorted(fixed_graph.input_nodes[t].edge_banks) == list(range(s_len))
-        assert sorted(fixed_graph.output_nodes[t].edge_banks) == list(range(s_len))
-        for s in range(s_len):
-            output_addr = get_output_address_at_input(t, s, fixed_graph.input_type, fixed_graph.output_type)
-            assert fixed_graph.input_nodes[t].edge_banks[s] == \
-                   fixed_graph.output_nodes[output_addr.t].edge_banks[output_addr.s]
+    check_banks(fixed_graph, s_len, t_len)
+
+def test_T4_S2_T4_banks():
+    t_len_1 = 4
+    t_len_2 = 4
+    s_len = 2
+    input_type = ST_TSeq(t_len_1, 0, ST_SSeq(s_len, ST_TSeq(t_len_2, 0, ST_Tombstone())))
+    output_type = ST_TSeq(t_len_2, 0, ST_TSeq(t_len_1, 0, ST_SSeq(s_len, ST_Tombstone())))
+    graph = build_input_output_graph(input_type, output_type)
+    fixed_graph = assign_banks(graph)
+    check_banks(fixed_graph, s_len, t_len_1*t_len_2)
 
 def test_T40_S6_T20_banks():
     t_len_1 = 40
@@ -42,7 +39,11 @@ def test_T40_S6_T20_banks():
     output_type = ST_TSeq(t_len_2, 0, ST_TSeq(t_len_1, 0, ST_SSeq(s_len, ST_Tombstone())))
     graph = build_input_output_graph(input_type, output_type)
     fixed_graph = assign_banks(graph)
-    for t in range(t_len_1 * t_len_2):
+    check_banks(fixed_graph, s_len, t_len_1*t_len_2)
+
+
+def check_banks(fixed_graph, s_len, t_len):
+    for t in range(t_len):
         # assert that all banks are used and are unique so no conflicts on input or output
         assert sorted(fixed_graph.input_nodes[t].edge_banks) == list(range(s_len))
         assert sorted(fixed_graph.output_nodes[t].edge_banks) == list(range(s_len))
@@ -50,3 +51,4 @@ def test_T40_S6_T20_banks():
             output_addr = get_output_address_at_input(t, s, fixed_graph.input_type, fixed_graph.output_type)
             assert fixed_graph.input_nodes[t].edge_banks[s] == \
                    fixed_graph.output_nodes[output_addr.t].edge_banks[output_addr.s]
+
