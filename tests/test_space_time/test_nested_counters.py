14829b77067e72c08d3db21555f3671ca2853299
David Durst <davidbdurst@gmail.com>
2019-09-06 17:10:52 -0700

    nested counter tseq 3 1 test passing without inner printing

--- tests/test_space_time/test_nested_counters.py
+++ tests/test_space_time/test_nested_counters.py
@@ -10,10 +10,10 @@ def test_nested_counters_TSeq_3_1():
     testcircuit = DefineNestedCounters(t)
     tester = fault.Tester(testcircuit, testcircuit.CLK)
 
+    tester.eval()
     for j in range(n+i):
         tester.print("valid: %d\n", tester.circuit.valid)
         tester.print("last: %d\n", tester.circuit.last)
-        tester.print("outer counter: %d\n", tester.circuit.Counter2_Mod4CE_inst0.O)
         tester.circuit.valid.expect(j < n)
         tester.circuit.last.expect(j == n+i - 1)
         tester.step(2)
