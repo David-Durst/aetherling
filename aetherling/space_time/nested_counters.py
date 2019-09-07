8d70cc02817baf2cfb2f8ff5bd275ba8aa659703
David Durst <davidbdurst@gmail.com>
2019-09-07 14:38:28 -0700

    adding valid index to counters, causing all tests to fail mysteriously

--- aetherling/space_time/nested_counters.py
+++ aetherling/space_time/nested_counters.py
@@ -2,6 +2,7 @@ from aetherling.space_time.space_time_types import *
 from aetherling.space_time.type_helpers import ST_Tombstone, is_nested
 from aetherling.modules.term_any_type import TermAnyType
 from mantle.coreir import DefineCoreirConst, DefineCoreirUlt
+from mantle.coreir.memory import getRAMAddrWidth
 from mantle.common.countermod import Decode
 from aetherling.modules.ram_any_type import *
 from aetherling.helpers.nameCleanup import cleanName
@@ -13,13 +14,18 @@ from typing import List
 __all__ = ['DefineNestedCounters', 'NestedCounters']
 
 @cache_definition
-def DefineNestedCounters(t: ST_Type, has_ce: bool = False, has_reset: bool = False) -> DefineCircuitKind:
+def DefineNestedCounters(t: ST_Type, has_cur_valid: bool = False, has_ce: bool = False, has_reset: bool = False) \
+            -> DefineCircuitKind:
     """
     Generate a set of nested counters that emit valid according to the specified type t.
     Last indicates whether this is the last clock (not period) of the type
+    has_cur_valid indicates whether this counter should also emit the current valid index among all the valids across
+    the nested types.
 
     valid: Out(Bit)
     last: Out(Bit)
+    if has_cur_valid
+    cur_valid: Out(Array[getRAMAddrWidth(t.valid_clocks()), Bit])
     if has_ce:
     CE : In(Bit)
     if has_reset:
@@ -29,14 +35,19 @@ def DefineNestedCounters(t: ST_Type, has_ce: bool = False, has_reset: bool = Fal
     class _NestedCounters(Circuit):
         name = 'NestedCounters_{}_hasCE{}_hasReset{}'.format(cleanName(str(t)), str(has_ce), str(has_reset))
         IO = ['valid', Out(Bit), 'last', Out(Bit)] + ClockInterface(has_ce=has_ce, has_reset=has_reset)
+        if has_cur_valid:
+            IO += ['cur_valid', Out(Array[getRAMAddrWidth(t.valid_clocks()), Bit])]
         @classmethod
         def definition(cls):
             if type(t) == ST_TSeq:
-                outer_counter = SizedCounterModM(t.n + t.i, has_ce= True, has_reset=has_reset)
-                inner_counters = DefineNestedCounters(t.t, has_ce, has_reset)()
+                outer_counter = SizedCounterModM(t.n + t.i, has_cur_valid=False, has_ce=True, has_reset=has_reset)
+                inner_counters = DefineNestedCounters(t.t, has_ce, has_reset, False)()
                 is_last = Decode(t.n + t.i - 1, outer_counter.O.N)(outer_counter.O)
                 valid_length = DefineCoreirConst(outer_counter.O.N, t.n)()
                 is_valid = DefineCoreirUlt(outer_counter.O.N)()
+                if has_cur_valid:
+                    cur_valid_counter = SizedCounterModM(t.valid_clocks(), has_ce=True, has_reset=has_reset)
+                    wire(cur_valid_counter.O, cls.cur_valid)
 
                 wire(is_valid.I0, outer_counter.O)
                 wire(is_valid.I1, valid_length.O)
@@ -46,13 +57,19 @@ def DefineNestedCounters(t: ST_Type, has_ce: bool = False, has_reset: bool = Fal
                 if has_reset:
                     wire(cls.RESET, outer_counter.RESET)
                     wire(cls.RESET, inner_counters.RESET)
+                    if has_cur_valid:
+                        wire(cls.RESET, cur_valid_counter.RESET)
                 if has_ce:
-                    wire(cls.CE, outer_counter.CE & inner_counters.last)
+                    wire(cls.CE & inner_counters.last, outer_counter.CE)
                     wire(cls.CE, inner_counters.CE)
+                    if has_cur_valid:
+                        wire(cls.CE & inner_counters.last, cur_valid_counter.CE)
                 else:
                     wire(inner_counters.last, outer_counter.CE)
+                    if has_cur_valid:
+                        wire(inner_counters.last, cur_valid_counter.CE)
             elif is_nested(t):
-                inner_counters = DefineNestedCounters(t.t, has_ce, has_reset)()
+                inner_counters = DefineNestedCounters(t.t, has_cur_valid, has_ce, has_reset)()
 
                 wire(inner_counters.valid, cls.valid)
                 wire(inner_counters.last, cls.last)
@@ -60,11 +77,15 @@ def DefineNestedCounters(t: ST_Type, has_ce: bool = False, has_reset: bool = Fal
                     wire(cls.RESET, inner_counters.RESET)
                 if has_ce:
                     wire(cls.CE, inner_counters.CE)
+                if has_cur_valid:
+                    wire(inner_counters.cur_valid, cls.cur_valid)
             else:
                 # only 1 element, so always last and valid element
                 valid_and_last = DefineCoreirConst(1, 1)()
                 wire(valid_and_last.O[0], cls.valid)
                 wire(valid_and_last.O[0], cls.last)
+                if has_cur_valid:
+                    wire(valid_and_last.O, cls.cur_valid)
 
     return _NestedCounters
 
