4cbb05c9ebdb8a39b8286288948f21b9bd1d8dd4
David Durst <davidbdurst@gmail.com>
2019-09-10 17:23:16 -0700

    fixed printing for reshape_st test, identified bug as ram_st not handling just atoms

--- tests/test_space_time/test_ram_st.py
+++ tests/test_space_time/test_ram_st.py
@@ -43,7 +43,7 @@ def test_ram_st_TSeq_3_1_SSeq_2():
 
     valid_clocks = [(o<no) for o in range(no+io)]
 
-    check_ram_st(no+io, num_t, tester, valid_clocks)
+    check_ram_st(no+io, num_t, tester, valid_clocks, ni)
 
 def test_ram_st_TSeq_3_1_SSeq_2_TSeq_2_2():
     no = 3
@@ -58,10 +58,18 @@ def test_ram_st_TSeq_3_1_SSeq_2_TSeq_2_2():
 
     valid_clocks = [(o < no) and (ii < nii) for o in range(no+io) for ii in range(nii+iii)]
 
-    check_ram_st((no+io) * (nii+iii), num_t, tester, valid_clocks)
+    check_ram_st((no+io) * (nii+iii), num_t, tester, valid_clocks, ni)
+
+def test_ram_st_Int():
+    t = ST_Int()
+    num_t = 5
+    valid_clocks = [True]
+    testcircuit = DefineRAM_ST(t, num_t)
+    tester = fault.Tester(testcircuit, testcircuit.CLK)
+    check_ram_st(1, num_t, tester, valid_clocks, None)
 
 
-def check_ram_st(clocks, num_t, tester, valid_clocks):
+def check_ram_st(clocks, num_t, tester, valid_clocks, ints_per_clock):
     tester.circuit.WE = True
     tester.circuit.RE = True
     clk = 0
@@ -69,14 +77,20 @@ def check_ram_st(clocks, num_t, tester, valid_clocks):
         for j in range(clocks):
             tester.print("clk: {}\n".format(clk))
             # tester.print("last: %d\n", tester.circuit.last)
-            tester.print("inner read counter: %d\n", tester.circuit.ir)
-            tester.print("inner write counter: %d\n", tester.circuit.iw)
+            #tester.print("inner read counter: %d\n", tester.circuit.ir)
+            #tester.print("inner write counter: %d\n", tester.circuit.iw)
             tester.circuit.WADDR = k
             tester.circuit.RADDR = k - 1
-            tester.circuit.WDATA = [2 * (j + k * clocks), 2 * (j + k * clocks) + 1]
+            if ints_per_clock is not None:
+                tester.circuit.WDATA = [ints_per_clock * (j + k * clocks) + i for i in range(ints_per_clock)]
+            else:
+                tester.circuit.WDATA = j + k * clocks
             tester.eval()
-            if k == 1 and valid_clocks[j]:
-                tester.circuit.RDATA.expect([2 * j, 2 * j + 1])
+            if k > 0 and valid_clocks[j]:
+                if ints_per_clock is not None:
+                    tester.circuit.RDATA.expect([ints_per_clock * (j + (k-1) * clocks) + i for i in range(ints_per_clock)])
+                else:
+                    tester.circuit.RDATA.expect(j + (k-1) * clocks)
             tester.step(2)
             clk += 1
     compile_and_run(tester)
--- tests/test_space_time/test_reshape_st.py
+++ tests/test_space_time/test_reshape_st.py
@@ -1,4 +1,5 @@
 from aetherling.modules.permutation.assign_banks import *
+from aetherling.modules.permutation import build_permutation_graph
 from aetherling.space_time.type_helpers import ST_Tombstone
 from aetherling.space_time.reshape_st import DefineReshape_ST
 from aetherling.helpers.fault_helpers import compile_and_run, compile
@@ -9,7 +10,7 @@ def test_2_3_flip_reshape():
     s_len = 2
     input_type = ST_SSeq(s_len, ST_TSeq(t_len, 0, ST_Int()))
     output_type = ST_TSeq(t_len, 0, ST_SSeq(s_len, ST_Int()))
-    graph = build_input_output_graph(input_type, output_type)
+    graph = build_permutation_graph(input_type, output_type)
     testcircuit = DefineReshape_ST(input_type, output_type)
     tester = fault.Tester(testcircuit, testcircuit.CLK)
     check_reshape(graph, 2, testcircuit.output_delay, tester)
@@ -26,16 +27,28 @@ def check_reshape(graph: InputOutputGraph, num_t, delay, tester, has_ce = False,
         for j in range(clocks + delay):
             tester.print("clk: {}\n".format(clk))
             # tester.print("last: %d\n", tester.circuit.last)
-            tester.eval()
             if j < clocks:
                 for k in range(len(graph.input_nodes[j].flat_idxs)):
                     tester.circuit.I[k] = graph.input_nodes[j].flat_idxs[k].idx
-            if j > delay:
-                for k in range(len(graph.output_nodes[output_counter].flat_idxs)):
-                    #tester.circuit.O[k].expect(graph.input_nodes[output_counter].flat_idxs[k].idx)
-                    tester.print("output {}: %d\n".format(k), tester.circuit.O[k])
+            tester.eval()
+            if j < clocks:
+                for k in range(len(graph.input_nodes[j].flat_idxs)):
+                    tester.print("ram_wr {}: %d\n".format(k), tester.circuit.ram_wr[k])
+                    tester.print("addr_wr {}: %d\n".format(k), tester.circuit.addr_wr[k])
+            for k in range(len(graph.output_nodes[output_counter].flat_idxs)):
+                #tester.circuit.O[k].expect(graph.input_nodes[output_counter].flat_idxs[k].idx)
+                tester.print("output {}: %d\n".format(k), tester.circuit.O[k])
+                tester.print("ram_rd {}: %d\n".format(k), tester.circuit.ram_rd[k])
+                tester.print("addr_rd {}: %d\n".format(k), tester.circuit.addr_rd[k])
+            if j >= delay:
                 output_counter += 1
+            tester.print("ram first valid write: %d\n", tester.circuit.first_valid)
             tester.step(2)
+            if j < clocks:
+                for k in range(len(graph.input_nodes[j].flat_idxs)):
+                    continue
+                    #tester.print("ram_wr {}: %d\n".format(k), tester.circuit.ram_wr[k])
+                    #tester.print("addr_wr {}: %d\n".format(k), tester.circuit.addr_wr[k])
             clk += 1
     tester.circuit.O[0].expect(1)
     compile_and_run(tester)
