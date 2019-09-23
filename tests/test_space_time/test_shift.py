d2089bf7176979244b5f86f20e006286209a0450
David Durst <davidbdurst@gmail.com>
2019-09-22 21:52:02 -0700

    removed unecessary shift_s test, shift_s passing all other tests

--- tests/test_space_time/test_shift.py
+++ tests/test_space_time/test_shift.py
@@ -37,59 +37,19 @@ def test_fault_rshift_parallel():
     shift_amount = 2
     in_type = ST_SSeq(num_in, ST_Int())
 
-    rshift = DefineShift_S(num_in, shift_amount, in_type.t)
+    rshift = DefineShift_S(num_in, shift_amount, in_type.t, has_valid=True)
 
     tester = fault.Tester(rshift, rshift.CLK)
 
-    tester.circuit.valid = 1
+    tester.circuit.valid_up = 1
     for i, val in enumerate(test_vals):
         tester.circuit.I[i] = val
     tester.eval()
     for i, val in enumerate(test_vals[shift_amount:]):
         tester.circuit.O[i + shift_amount].expect(test_vals[i])
-    tester.circuit.I.expect(1)
+    tester.circuit.valid_down.expect(1)
     compile_and_run(tester)
 """
-def test_rshift_parallel_alternate_rv():
-    width = 5
-    num_in = 4
-    test_vals = [2,5,3,8]
-    shift_amount = 2
-    num_clocks = 6
-    scope = Scope()
-    in_type = Array[num_in, Array[width, In(BitIn)]]
-    args = ['I', in_type, 'O', Out(in_type), 'ready_up', Out(Bit), 'valid_up', In(Bit), 'ready_down', In(Bit),
-            'valid_down', Out(Bit)] + ClockInterface(False, False)
-
-    testcircuit = DefineCircuit('Test', *args)
-
-    rshift = RShiftParallel(num_in, shift_amount, in_type.T, True)
-    wire(rshift.I, testcircuit.I)
-    wire(testcircuit.O, rshift.O)
-    wire(testcircuit.ready_up, rshift.ready_up)
-    wire(testcircuit.valid_up, rshift.valid_up)
-    wire(testcircuit.ready_down, rshift.ready_down)
-    wire(testcircuit.valid_down, rshift.valid_down)
-
-    EndCircuit()
-
-    sim = CoreIRSimulator(testcircuit, testcircuit.CLK)
-
-    sim.evaluate()
-    for clk in range(num_clocks):
-        for i, val in enumerate(test_vals):
-            sim.set_value(testcircuit.I[i], int2seq(val + clk * 3, width), scope)
-        sim.set_value(testcircuit.valid_up, clk % 3 == 0, scope)
-        sim.set_value(testcircuit.ready_down, clk % 2 == 0, scope)
-        sim.evaluate()
-        for i, val in enumerate(test_vals[shift_amount:]):
-            assert seq2int(sim.get_value(testcircuit.O[i + shift_amount])) == test_vals[i] + clk * 3
-        assert sim.get_value(testcircuit.valid_down, scope) == (clk % 3 == 0)
-        assert sim.get_value(testcircuit.ready_up, scope) == (clk % 2 == 0)
-        sim.advance_cycle()
-        sim.evaluate()
-
-
 def test_rshift_sequential_rv_always_true_no_ce():
     width = 5
     test_vals = [2,5,3,8]
