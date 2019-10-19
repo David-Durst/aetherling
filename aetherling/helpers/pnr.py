3190416ae6ddba324489eff1b8a7fd2bce58d97b
David Durst <davidbdurst@gmail.com>
2019-10-19 12:31:07 -0700

    merge columns for making percent comparsion tables

--- aetherling/helpers/pnr.py
+++ aetherling/helpers/pnr.py
@@ -1,9 +1,10 @@
 import pandas as pd
-from math import isnan
+from math import isnan, nan
 from fractions import Fraction as frac
 
 def get_latex_from_results_str(results_file):
     results = pd.read_csv(results_file)
+    results['Clock Rate'] = nan
     results_tex_str = ""
     systems = ["aetherling_copies", "halide_to_hardware", "spatial"]
     applications = ["map", "pyramid", "sharpen", "camera"]
@@ -29,15 +30,17 @@ def get_latex_from_results_str(results_file):
 
     # get all Aetherling results into latex tables
     #aetherling_per_app = per_system_per_application_results[0]
-    for system_per_app in per_system_per_application_results:
-        for app_pd in system_per_app:
+    for i, system_per_app in enumerate(per_system_per_application_results):
+        for j, app_pd in enumerate(system_per_app):
+            results_tex_str += "System {}, App {}\n".format(systems[i], applications[j])
             results_tex_str += app_pd.to_latex(index=False, escape=False)
-#    for app_idx in range(len(applications)):
-#        results_tex_str += merge_columns(
-#            per_system_per_application_results[0][app_idx],
-#            per_system_per_application_results[1][app_idx],
-#            per_system_per_application_results[2][app_idx],
-#        ).to_latex(index=False, escape=False)
+    for app_idx in range(len(applications)):
+        results_tex_str += "Comparison for App {}\n".format(applications[j])
+        results_tex_str += merge_columns(
+            per_system_per_application_results[0][app_idx],
+            per_system_per_application_results[1][app_idx],
+            per_system_per_application_results[2][app_idx],
+        ).to_latex(index=False, escape=False)
     return results_tex_str
 
 
@@ -57,22 +60,27 @@ def get_output_columns(results_pd):
     results_pd['BRAMs'] = results_pd['BRAMs'].apply(int_if_not_nan)
     results_pd['Slices'] = results_pd['Slices'].apply(int_if_not_nan)
     results_pd['Parallelism'] = results_pd['Parallelism'].apply(int_if_not_nan)
-    return results_pd[['Parallelism', 'LUTs', 'BRAMs', 'Slices']]
+    results_pd['Clock Rate'] = results_pd['Clock Rate'].apply(int_if_not_nan)
+    return results_pd[['Parallelism', 'LUTs', 'BRAMs', 'Slices', 'Clock Rate']]
 
-def merge_columns(aetherling_results, atial_results):
+def percent_vs_aetherling(aetherling_results, other_results, column_name):
+    others = pd.to_numeric(other_results[column_name], errors='coerce')
+    ae = pd.to_numeric(aetherling_results[column_name], errors='coerce')
+    return ((others - ae) / ae).apply(int_if_not_nan)
+
+def merge_columns(aetherling_results, halide_results, spatial_results):
     aetherling_results['Aetherling\nLUTs'] = aetherling_results['LUTs']
     aetherling_results['Aetherling\nBRAMs'] = aetherling_results['BRAMs']
     aetherling_results['Aetherling\nSlices'] = aetherling_results['Slices']
-    aetherling_results['Halide-To-Hardware\nLUTs'] = halide_results['LUTs']
-    aetherling_results['Halide-To-Hardware\nBRAMs'] = halide_results['BRAMs']
-    aetherling_results['Halide-To-Hardware\nSlices'] = halide_results['Slices']
-    aetherling_results['Spatial\nLUTs'] = spatial_results['LUTs']
-    aetherling_results['Spatial\nBRAMs'] = spatial_results['BRAMs']
-    aetherling_results['Spatial\nSlices'] = spatial_results['Slices']
+    aetherling_results['HLUTs'] = percent_vs_aetherling(aetherling_results, halide_results, 'LUTs')
+    aetherling_results['HBRAMs'] = percent_vs_aetherling(aetherling_results, halide_results, 'BRAMs')
+    aetherling_results['HSlices'] = percent_vs_aetherling(aetherling_results, halide_results, 'Slices')
+    aetherling_results['SLUTs'] = percent_vs_aetherling(aetherling_results, spatial_results, 'LUTs')
+    aetherling_results['SBRAMs'] = percent_vs_aetherling(aetherling_results, spatial_results, 'BRAMs')
+    aetherling_results['SSlices'] = percent_vs_aetherling(aetherling_results, spatial_results, 'Slices')
     return aetherling_results[['Parallelism',
-                               'Aetherling\nLUTs', 'Halide-To-Hardware\nLUTs', 'Spatial\nLUTs',
-                               'Aetherling\nBRAMs', 'Halide-To-Hardware\nBRAMs', 'Spatial\nBRAMs',
-                               'Aetherling\nSlices', 'Halide-To-Hardware\nSlices', 'Spatial\nSlices'
+                               'HLUTs', 'HBRAMs', 'HSlices',
+                               'SLUTs', 'SBRAMs', 'SSlices',
                                ]]
 
 
