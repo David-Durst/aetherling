cc145f0d1882b7a8db4c9ef32064286bb0a3d5eb
David Durst <davidbdurst@gmail.com>
2019-10-23 18:14:05 -0700

    working towards serialize

--- aetherling/space_time/modules/serialize.py
+++ aetherling/space_time/modules/serialize.py
@@ -4,19 +4,22 @@ from magma.frontend.coreir_ import CircuitInstanceFromGeneratorWrapper, GetCoreI
 from aetherling.helpers.nameCleanup import cleanName
 from magma.t import Kind
 from aetherling.modules.term_any_type import DefineTermAnyType
-from aetherling.space_time.type_helpers import valid_ports
+from aetherling.space_time.type_helpers import *
+from aetherling.space_time.ram_st import DefineRAM_ST
 from aetherling.modules import DefineRAMAnyType, DefineRegisterAnyType, DefineMuxAnyType, DefineNativeMapParallel
 from mantle.common.countermod import SizedCounterModM
 from mantle.common.decode import Decode
 from mantle.coreir import DefineSub
-from mantle.coreir import DefineCoreirConst
+import math
 
 @cache_definition
-def DefineSerializer(n:int, i:int, T: Kind, has_reset=False) -> DefineCircuitKind:
+def DefineSerialize(n:int, i:int, T: ST_Type,  has_reset=False) -> DefineCircuitKind:
     """
     Convert sequences in space to sequences in time.
-    TSeq 1 (n-1+i) (SSeq n T) -> TSeq n i T
-    Each T must only be in space, not time
+    TSeq no (vo+no*vi) (SSeq n T') -> TSeq no vo (TSeq n vi T')
+
+    Each T' period is time_per_element clock cycles
+    You can get time_per_element by calling time on a space-time type.
 
     Note that the T passed to this operator just the Magma type emitted each clock cycle.
     You can get T by calling magma_repr on a space-time type T'.
@@ -32,60 +35,79 @@ def DefineSerializer(n:int, i:int, T: Kind, has_reset=False) -> DefineCircuitKin
     if has_reset:
     RESET : In(Bit)
     """
-    class _Serializer(Circuit):
-        name = "serialize_t{}_tEl{}_n{}_hasCE{}_hasRESET{}".format(cleanName(str(T)), str(n), \
-                                                                   str(i), str(has_reset))
-
-        IO = ["I", In(Array[n, T]), "O", Out(T)] + \
-             ClockInterface(has_ce=False, has_reset=has_reset) + valid_ports
+    class _Serialize(Circuit):
+        name = "serialize_t{}_n{}_i{}_hasRESET{}".format(cleanName(str(T)), \
+                                                                   str(n), str(i), str(has_reset))
+        st_in_t = [ST_TSeq(1, n-1+i, ST_SSeq_Tuple(n, T))]
+        st_out_t = ST_TSeq(n, i, T)
+        IO = ["I", st_in_t[0].magma_repr(), "O", st_out_t.magma_repr()] + \
+             ClockInterface(False, has_reset) + valid_ports
 
         @classmethod
         def definition(cls):
             # the counter of the current element of output sequence, when hits 0, load the next input to serialize
             element_idx_counter = SizedCounterModM(n+i, has_ce=True, has_reset=has_reset)
-            is_first_element = Decode(0, element_idx_counter.O.N)(element_idx_counter.O)
+            if element_idx_counter.O.N == math.ceil(math.log(n, 2)):
+                element_idx_out = element_idx_counter.O
+            else:
+                used_bits_length = (math.ceil(math.log(n, 2)))
+                unused_bits_length = element_idx_counter.O.N - used_bits_length
+                element_idx_out = element_idx_counter.O[:used_bits_length-1]
+                term = DefineTermAnyType(Array[unused_bits_length, Bit])
+                wire(element_idx_counter.O[used_bits_length:], term.I)
+            is_first_element = Decode(0, element_idx_out.N)(element_idx_out)
 
+            enabled = cls.valid_up
             wire(cls.valid_up, cls.valid_down)
 
-            if has_reset:
-                wire(cls.RESET, element_idx_counter.RESET)
+            # if each element takes multiple clocks, need a ram so can write all them and read them over multiple clocks
+            if is_nested(T) and T.time() > 1:
+                value_store = DefineNativeMapParallel(n, DefineRAMAnyType(T.magma_repr(), T.time()))()
+                value_store_input = value_store.WDATA
+                value_store_output = value_store.RDATA
 
-            value_store = DefineNativeMapParallel(n-1, DefineRegisterAnyType(T, has_ce=True))()
-            value_store_input = value_store.I
-            value_store_output = value_store.O
+                time_per_element_counter = SizedCounterModM(T.time(),
+                                                            has_ce=True, has_reset=has_reset)
+                go_to_next_element = Decode(T.time() - 1, time_per_element_counter.O.N)(time_per_element_counter.O)
 
-            wire(element_idx_counter.CE, cls.valid_up)
-            for input_idx in range(n):
-                wire(value_store.CE[input_idx], is_first_element & cls.valid_up)
+                wire(time_per_element_counter.CE, enabled)
+                wire(element_idx_counter.CE, enabled & go_to_next_element)
+                for input_idx in range(n):
+                    wire(value_store.WE[input_idx], is_first_element & enabled)
+                    # location in current element is where to read and write.
+                    # will write on first iteration through element, read on later iterations
+                    wire(time_per_element_counter.O, value_store.WADDR[input_idx])
+                    wire(time_per_element_counter.O, value_store.RADDR[input_idx])
+
+                if has_reset:
+                    wire(time_per_element_counter.RESET, cls.RESET)
+
+            else:
+                value_store = DefineNativeMapParallel(n, DefineRegisterAnyType(T.magma_repr(), has_ce=True))()
+                value_store_input = value_store.I
+                value_store_output = value_store.O
+
+                wire(element_idx_counter.CE, enabled)
+                for input_idx in range(n):
+                    wire(value_store.CE[input_idx], is_first_element & enabled)
+
 
             wire(cls.I, value_store_input)
 
             # to serialize, go through all different rams/registers in value store
             # and select the output from the ith one, where i is current output element
-            value_store_output_selector = DefineMuxAnyType(T, n-1)()
+            value_store_output_selector = DefineMuxAnyType(T.magma_repr(), n)()
             wire(value_store_output, value_store_output_selector.data)
-            sub1 = DefineSub(element_idx_counter.O.N)()
-            wire(sub1.I0, element_idx_counter.O)
-            one_const = DefineCoreirConst(element_idx_counter.O.N, 1)
-            wire(sub1.I1, one_const.O)
-            if element_idx_counter.O.N == value_store_output_selector.sel.N:
-                wire(sub1.O, value_store_output_selector.sel)
-            else:
-                used_bits_length = value_store_output_selector.sel.N
-                unused_bits_length = sub1.O.N - used_bits_length
-                wire(sub1.O[:used_bits_length-1], value_store_output_selector.sel)
-                term = DefineTermAnyType(Array[unused_bits_length, Bit])
-                wire(sub1.O[used_bits_length:], term.I)
-
+            wire(element_idx_out, value_store_output_selector.sel)
 
             # on first element, send the input directly out. otherwise, use the register
-            first_element_output_selector = DefineMuxAnyType(T, 2)()
+            first_element_output_selector = DefineMuxAnyType(T.magma_repr(), 2)()
             wire(is_first_element, first_element_output_selector.sel[0])
             wire(value_store_output_selector, first_element_output_selector.data[0])
             wire(cls.I[0], first_element_output_selector.data[1])
             wire(first_element_output_selector.out, cls.O)
 
-    return _Serializer
+    return _Serialize
 
-def Serializer(n:int, time_per_element: int, T: Kind, has_ce=False, has_reset=False) -> Circuit:
-    return DefineSerializer(n, time_per_element, T, has_ce, has_reset)()
+def Serialize(n:int, time_per_element: int, T: Kind, has_ce=False, has_reset=False) -> Circuit:
+    return DefineSerialize(n, time_per_element, T, has_ce, has_reset)()
---
+++ tests/test_space_time/test_serialize.py
@@ -0,0 +1,29 @@
+from aetherling.space_time import *
+from magma import *
+from magma.clock import *
+from magma.bitutils import *
+import fault
+from aetherling.helpers.fault_helpers import compile_and_run, wrap_module_with_top
+from aetherling.space_time.modules.serialize import DefineSerialize
+
+def test_serialize_basic():
+    n = 5
+    i = 0
+    inputs = list(range(n))
+    num_iterations = 2
+    t = ST_Int()
+
+
+    ser = DefineSerialize(n, i, t)
+
+    tester = fault.Tester(ser, ser.CLK)
+
+    tester.circuit.valid_up = 1
+    #tester.circuit.I = inputs
+    for clk in range(num_iterations):
+        tester.print("clk: {}\n".format(clk))
+        tester.eval()
+        tester.circuit.valid_down.expect(1)
+        #tester.circuit.O[0].expect(inputs[clk % n])
+        tester.step(2)
+    compile_and_run(tester)
