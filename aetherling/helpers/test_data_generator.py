a0ccda744b41ba4390b41b2aadc93f297b016999
David Durst <davidbdurst@gmail.com>
2020-02-27 23:47:54 -0800

    11 seconds for generating test data

---
+++ aetherling/helpers/test_data_generator.py
@@ -0,0 +1,36 @@
+input_data = [i for i in range(1920*1080)]
+
+def stencil_generator(row_size, inputs_2d):
+  col_size = len(inputs_2d) // row_size
+  num_rows = col_size
+  num_cols = row_size
+  def get_input(r, c):
+      if ((r < 0) or (c < 0)):
+        return 253
+      else:
+        return inputs_2d[r * row_size + c]
+  return [
+      [
+          [
+            get_input(r - stencil_r, c - stencil_c)
+              for stencil_c in [2,1,0]
+          ] for stencil_r in [2,1,0]
+      ] for r in range(num_rows) for c in range(num_cols)
+  ]
+
+hask_kernel = [1,2,1,2,4,2,1,2,1]
+def conv_generator(stencil_2d_output):
+    result = []
+    for window in stencil_2d_output:
+        flat_window = window[0] + window[1] + window[2]
+        if 253 in flat_window:
+            result.append(253)
+        else:
+            mac = 0
+            for i in range(9):
+                mac += hask_kernel[i] * flat_window[i]
+            result.append(mac % 256 // 16)
+    return result
+
+
+print(str(sum(conv_generator(stencil_generator(1920, [i for i in range(1920*1080)])))))
